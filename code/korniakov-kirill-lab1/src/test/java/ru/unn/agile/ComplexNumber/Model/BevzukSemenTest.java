87b0d3766aa29ebd32a883c953d569277f64e727
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-10-31 20:53:26 +0300

    Add tests for the constructor

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/BevzukSemenTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/BevzukSemenTest.java
@@ -6,16 +6,33 @@ import ru.unn.agile.ComplexNumber.model.ComplexNumber;
 import static org.junit.Assert.*;
 
 public class BevzukSemenTest {
+    private final double delta = 0.001;
+
     @Test
-    public void canAddComplexNumbers() {
-        // Arrange
-        ComplexNumber z1 = new ComplexNumber(1, 0);
-        ComplexNumber z2 = new ComplexNumber(0, 1);
+    public void canCreateComplexNumberByDefault(){
+        ComplexNumber a = new ComplexNumber();
 
-        // Act
-        ComplexNumber z = z1.add(z2);
+        assertNotNull(a);
+    }
 
-        // Assert
-        assertEquals(new ComplexNumber(1, 1), z);
+    @Test
+    public void canCreateComplexNumberWithParameters(){
+        ComplexNumber a = new ComplexNumber(1.0, 2.0);
+
+        assertNotNull(a);
+    }
+
+    @Test
+    public void canSetInitialReal(){
+        ComplexNumber a = new ComplexNumber(1.0, 2.0);
+
+        assertEquals(1.0, a.getReal(), delta);
+    }
+
+    @Test
+    public void canSetInitialImaginary(){
+        ComplexNumber a = new ComplexNumber(1.0, 2.0);
+
+        assertEquals(2.0, a.getImaginary(), delta);
     }
 }
