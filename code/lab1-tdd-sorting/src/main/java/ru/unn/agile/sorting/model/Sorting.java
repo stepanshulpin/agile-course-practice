2822ceab0f3ca811b5b4b632956698b6ed3e1659
Selllavi <intriganchik@yandex.ru>
2019-11-02 18:17:54 +0300

    sorting

--- code/lab1-tdd-sorting/src/main/java/ru/unn/agile/sorting/model/Sorting.java
+++ code/lab1-tdd-sorting/src/main/java/ru/unn/agile/sorting/model/Sorting.java
@@ -8,11 +8,14 @@ public final class Sorting {
         direction = Direction.ASC;
     }
 
-    public Sorting(Direction direction) {
+    public Sorting(final Direction direction) {
         this.direction = direction;
     }
 
-    public int[] sort(int[] numbers) {
+    public int[] sort(final int[] numbers) {
+        if (numbers.length == 2) {
+            sortTwoNumbers(numbers);
+        }
         if (direction.equals(Direction.ASC)) {
             return ascSort(numbers);
         } else {
@@ -20,18 +23,51 @@ public final class Sorting {
         }
     }
 
-    public static int[] ascSort(int[] numbers) {
+    private int[] ascSort(final int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            int current = numbers[i];
+            int j = i - 1;
+            while (j >= 0 && current < numbers[j]) {
+                numbers[j + 1] = numbers[j];
+                j--;
+            }
+            numbers[j + 1] = current;
+        }
+        return numbers;
+    }
+
+    private int[] descSort(final int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            int current = numbers[i];
+            int j = i - 1;
+            while (j >= 0 && current > numbers[j]) {
+                numbers[j + 1] = numbers[j];
+                j--;
+            }
+            numbers[j + 1] = current;
+        }
+        return numbers;
+    }
+
+    public int[] sortTwoNumbers(final int[] numbers) {
+        if (direction.equals(Direction.ASC)) {
+            return ascSortTwoNumbers(numbers);
+        } else {
+            return descSortTwoNumbers(numbers);
+        }
+    }
+
+    public static int[] ascSortTwoNumbers(final int[] numbers) {
         if (numbers[1] > numbers[0]) {
-            return new int[]{numbers[0], numbers[1]};
+            return numbers;
         }
         return new int[]{numbers[1], numbers[0]};
     }
 
-    public static int[] descSort(int[] numbers) {
+    public static int[] descSortTwoNumbers(final int[] numbers) {
         if (numbers[0] > numbers[1]) {
-            return new int[]{numbers[0], numbers[1]};
+            return numbers;
         }
         return new int[]{numbers[1], numbers[0]};
     }
-
 }
