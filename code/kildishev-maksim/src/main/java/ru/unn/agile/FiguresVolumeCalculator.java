dc707294ea1975d3183b7f25602a2083e96568bb
Maksim Kildishev <thanatormax@gmail.com>
2019-11-02 23:14:28 +0300

    Few test for pyramidVolumeCalculate method added

--- code/kildishev-maksim/src/main/java/ru/unn/agile/FiguresVolumeCalculator.java
+++ code/kildishev-maksim/src/main/java/ru/unn/agile/FiguresVolumeCalculator.java
@@ -1,4 +1,12 @@
 package ru.unn.agile;
 
 public class FiguresVolumeCalculator {
+
+    public static double pyramidVolumeCalculate(final double baseSquare, final double height) {
+        return 1./3 * baseSquare * height;
+    }
+
+    public static double sphereVolumeCalculate(final double radius) {
+        return 0;
+    }
 }
--- code/kildishev-maksim/src/test/java/ru/unn/agile/FiguresVolumeCalculatorTest.java
+++ code/kildishev-maksim/src/test/java/ru/unn/agile/FiguresVolumeCalculatorTest.java
@@ -1,6 +1,37 @@
 package ru.unn.agile;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
+
 public class FiguresVolumeCalculatorTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void canCalculatePyramidVolume() {
+        assertEquals(3, FiguresVolumeCalculator.pyramidVolumeCalculate(3, 3), 0.01);
+    }
+
+    @Test
+    public void canThrowExceptionIfSquareIsNegative() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Square can't be negative");
+        FiguresVolumeCalculator.pyramidVolumeCalculate(-3, 3);
+    }
+
+    @Test
+    public void canCalculatePyramidVolumeWithNegativeHeight() {
+        assertEquals(3, FiguresVolumeCalculator.pyramidVolumeCalculate(3, -3), 0.01);
+    }
+
+    @Test
+    public void canCalculateSphereVolume() {
+        //assertEquals();
+    }
 
 
 }
