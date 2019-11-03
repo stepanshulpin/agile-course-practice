221313aff2b834ea1505e49028cc4e6e5d6a13cc
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-03 23:06:31 +0300

    Fix RedundantImport

--- code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/TemperatureConverter.java
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/TemperatureConverter.java
@@ -1,8 +1,5 @@
 package ru.unn.agile.TemperatureConverter.model;
 
-import ru.unn.agile.TemperatureConverter.model.DegreeCelsius;
-import ru.unn.agile.TemperatureConverter.model.DegreeFahrenheit;
-
 public class TemperatureConverter {
     public DegreeFahrenheit convertCelsiusToFahrenheit(final DegreeCelsius celsius) {
         final double coefficient = 9.0 / 5.0;
