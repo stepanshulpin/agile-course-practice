400ee36dedc82b4c99825e32a8d233e6f374af14
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-03 22:43:09 +0300

    Fix whitespace

--- code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/DegreeCelsius.java
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/DegreeCelsius.java
@@ -4,8 +4,8 @@ public class DegreeCelsius {
     private double temperature;
     private final double absoluteZero = -273.15;
 
-    public DegreeCelsius(final double temperature) throws IllegalArgumentException{
-        if(temperature < this.absoluteZero){
+    public DegreeCelsius(final double temperature) throws IllegalArgumentException {
+        if (temperature < this.absoluteZero) {
             throw new IllegalArgumentException("The temperature is less than absolute zero!");
         }
         this.temperature = temperature;
