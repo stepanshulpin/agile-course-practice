46b1ec65b3be813ca999608f920f984e0df55eb7
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-03 22:09:28 +0300

    Add class DegreeCelsius and test canCreateDegreeСelsius0

---
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/DegreeCelsius.java
@@ -0,0 +1,13 @@
+package ru.unn.agile.TemperatureConverter.model;
+
+public class DegreeCelsius {
+    private double temperature;
+
+    public DegreeCelsius(final double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getTemperature() {
+        return this.temperature;
+    }
+}
---
+++ code/temperature-converter/src/test/java/ru/unn/agile/TemperatureConverter/Model/DegreeCelsiusTest.java
@@ -0,0 +1,17 @@
+package ru.unn.agile.TemperatureConverter.Model;
+
+import org.junit.Test;
+import ru.unn.agile.TemperatureConverter.model.DegreeCelsius;
+
+import static org.junit.Assert.*;
+
+public class DegreeCelsiusTest {
+    private final double delta = 0.001;
+
+    @Test
+    public void canCreateDegreeCelsius0() {
+        DegreeCelsius celsius = new DegreeCelsius(0.0);
+
+        assertEquals(0.0, celsius.getTemperature(), delta);
+    }
+}
