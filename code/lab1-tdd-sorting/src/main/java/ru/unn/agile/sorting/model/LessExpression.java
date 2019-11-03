7797e205a0c2f33728972eae563080a2319f61ec
Selllavi <intriganchik@yandex.ru>
2019-11-03 16:04:52 +0300

    sorting

--- code/lab1-tdd-sorting/src/main/java/ru/unn/agile/sorting/model/LessExpression.java
+++ code/lab1-tdd-sorting/src/main/java/ru/unn/agile/sorting/model/LessExpression.java
@@ -3,7 +3,7 @@ package ru.unn.agile.sorting.model;
 public class LessExpression extends Expression {
 
     @Override
-    public boolean compare(final int number1,final int number2) {
+    public boolean compare(final int number1, final int number2) {
         return number1 < number2;
     }
 }
