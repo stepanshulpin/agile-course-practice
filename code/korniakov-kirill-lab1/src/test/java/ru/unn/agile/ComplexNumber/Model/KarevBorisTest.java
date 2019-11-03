6fbe7d886aa1d0ba8b43075f9e78d5e38e36d1dc
karevboris <karevboris333@gmail.com>
2019-11-04 00:04:21 +0300

    Deleted duplicate

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KarevBorisTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KarevBorisTest.java
@@ -22,20 +22,6 @@ public class KarevBorisTest {
     }
 
     @Test
-    public void canAddComplexNumbers() {
-        // Arrange
-        ComplexNumber z1 = new ComplexNumber(1, 0);
-        ComplexNumber z2 = new ComplexNumber(0, 1);
-
-        // Act
-        ComplexNumber z = z1.add(z2);
-
-        // Assert
-        assertEquals(new ComplexNumber(1, 1), z);
-    }
-
-
-    @Test
     public void canCreateComplexNumberWithInitialValues() {
         ComplexNumber number = new ComplexNumber(1, 1);
         assertNotNull(number);
