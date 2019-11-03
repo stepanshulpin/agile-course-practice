e519b1035e0846c1400e2f31d9ee091ac22518f0
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-03 21:23:45 +0300

    Fix MagicNumbers again

--- code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/TemperatureConverter.java
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/TemperatureConverter.java
@@ -1,9 +1,9 @@
 package ru.unn.agile.TemperatureConverter.model;
 
 public class TemperatureConverter {
-    public double convertCelsiusToFahrenheit(double celsius) {
-        double coefficient = 9.0 / 5.0;
-        double shift = 32.0;
+    public double convertCelsiusToFahrenheit(final double celsius) {
+        final double coefficient = 9.0 / 5.0;
+        final double shift = 32.0;
         return coefficient * celsius + shift;
     }
 }
