a239181d30812c3cbee102e482dfa5dcf9168eaa
Anna-Mironova <anyt.ka@bk.ru>
2019-11-04 16:56:41 +0300

    Refactoring

---
+++ code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/DistributionChecker.java
@@ -0,0 +1,21 @@
+package ru.unn.agile.StatisticsCalculation.model;
+
+public class DistributionChecker {
+    public static void checkArraysInitialization(final Number[] array){
+        if (array == null) {
+            throw new IllegalArgumentException("Array should be initialized!");
+        }
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Array should be initialized!");
+        }
+        for (int i = 0; i< array.length;i++){
+            if (array[i] == null) throw new IllegalArgumentException("Array should be initialized!");
+        }
+    }
+
+    public static void checkArraysSize(final Number[] values, final Double[] probabilities){
+        if (values.length != probabilities.length) {
+            throw new IllegalArgumentException("Values and probabilities arrays should have the same dimension!");
+        }
+    }
+}
--- code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
+++ code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
@@ -1,28 +1,10 @@
 package ru.unn.agile.StatisticsCalculation.model;
 
 public class StatisticsCalculation {
-    private static void checkArraysInitialization(final Number[] array){
-        if (array == null) {
-            throw new IllegalArgumentException("Array should be initialized!");
-        }
-        if (array.length == 0) {
-            throw new IllegalArgumentException("Array should be initialized!");
-        }
-        for (int i = 0; i< array.length;i++){
-            if (array[i] == null) throw new IllegalArgumentException("Array should be initialized!");
-        }
-    }
-
-    private static void checkArraysSize(final Number[] values, final Double[] probabilities){
-        if (values.length != probabilities.length) {
-            throw new IllegalArgumentException("Values and probabilities arrays should have the same dimension!");
-        }
-    }
-
     public static double calculateExpectedValue(final Number[] values, final Double[] probabilities) {
-        checkArraysInitialization(values);
-        checkArraysInitialization(probabilities);
-        checkArraysSize(values, probabilities);
+        DistributionChecker.checkArraysInitialization(values);
+        DistributionChecker.checkArraysInitialization(probabilities);
+        DistributionChecker.checkArraysSize(values, probabilities);
         Double result = 0.0;
         for (int i = 0; i < values.length; i++) {
             result += values[i].doubleValue() * probabilities[i];
