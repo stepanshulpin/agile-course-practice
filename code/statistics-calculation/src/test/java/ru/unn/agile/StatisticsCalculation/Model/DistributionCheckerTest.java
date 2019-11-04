7f55a6913d7d476d3ee3bc259c99913f5b300bce
Anna-Mironova <anyt.ka@bk.ru>
2019-11-04 17:12:24 +0300

    Rename methods in new test class

--- code/statistics-calculation/src/test/java/ru/unn/agile/StatisticsCalculation/Model/DistributionCheckerTest.java
+++ code/statistics-calculation/src/test/java/ru/unn/agile/StatisticsCalculation/Model/DistributionCheckerTest.java
@@ -5,7 +5,7 @@ import ru.unn.agile.StatisticsCalculation.model.DistributionChecker;
 
 public class DistributionCheckerTest {
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForEmptyValuesAndProbs() {
+    public void throwWhenValidateEmptyValuesAndProbs() {
         Integer[] values = {};
         Double[] probabilities = {};
         DistributionChecker checker = new DistributionChecker();
@@ -14,7 +14,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForEmptyValues() {
+    public void throwWhenValidateEmptyValues() {
         Integer[] values = {};
         Double[] probabilities = {1.0};
         DistributionChecker checker = new DistributionChecker();
@@ -23,7 +23,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForEmptyProbs() {
+    public void throwWhenValidateEmptyProbs() {
         Integer[] values = {1};
         Double[] probabilities = {};
         DistributionChecker checker = new DistributionChecker();
@@ -32,7 +32,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForNullValuesAndProbs() {
+    public void throwWhenValidateNullValuesAndProbs() {
         Integer[] values = null;
         Double[] probabilities = null;
         DistributionChecker checker = new DistributionChecker();
@@ -41,7 +41,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForNullValues() {
+    public void throwWhenValidateNullValues() {
         Integer[] values = null;
         Double[] probabilities = {1.0};
         DistributionChecker checker = new DistributionChecker();
@@ -50,7 +50,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForNullProbs() {
+    public void throwWhenValidateNullProbs() {
         Integer[] values = {1};
         Double[] probabilities = null;
         DistributionChecker checker = new DistributionChecker();
@@ -59,7 +59,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForNotEqualsValsAndProbs() {
+    public void throwWhenValidateValsAndProbsWithDifferentSize() {
         Integer[] values = {1, 2, 3};
         Double[] probabilities = {0.1, 0.9};
         DistributionChecker checker = new DistributionChecker();
@@ -68,7 +68,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForValuesWithNull() {
+    public void throwWhenValidateValuesWithNull() {
         Integer[] values = {1, null, 3};
         Double[] probabilities = {0.1, 0.8, 0.1};
         DistributionChecker checker = new DistributionChecker();
@@ -77,7 +77,7 @@ public class DistributionCheckerTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void throwWhenCalculateExpectedValueForProbsWithNull() {
+    public void throwWhenValidateProbsWithNull() {
         Integer[] values = {1, 2, 3};
         Double[] probabilities = {0.1, null, 0.1};
         DistributionChecker checker = new DistributionChecker();
