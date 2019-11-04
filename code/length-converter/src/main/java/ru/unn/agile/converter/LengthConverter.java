9f052f9dc54e088344a78ea0d25bea158d933d98
karevboris <karevboris333@gmail.com>
2019-11-04 15:16:39 +0300

    Added tests + funcs

--- code/length-converter/src/main/java/ru/unn/agile/converter/LengthConverter.java
+++ code/length-converter/src/main/java/ru/unn/agile/converter/LengthConverter.java
@@ -2,11 +2,21 @@ package ru.unn.agile.converter;
 
 public final class LengthConverter {
     private static final double METER_TO_CENTIMETER_COEFF = 100;
+    private static final double METER_TO_KILOMETER_COEFF = 1000;
+    private static final double METER_TO_MILLIMETER_COEFF = 1000;
 
     private LengthConverter() {
     }
 
     public static double meterToCentimeter(final double meter) {
         return meter * METER_TO_CENTIMETER_COEFF;
+    }
+
+    public static double meterToKilometer(final double meter) {
+        return meter / METER_TO_KILOMETER_COEFF;
+    }
+
+    public static double meterToMillimeter(final double meter) {
+        return meter * METER_TO_MILLIMETER_COEFF;
     }
 }
--- code/length-converter/src/test/java/ru/unn/agile/converter/LengthConverterTest.java
+++ code/length-converter/src/test/java/ru/unn/agile/converter/LengthConverterTest.java
@@ -13,4 +13,16 @@ public class LengthConverterTest {
         double cm = LengthConverter.meterToCentimeter(5);
         assertEquals(500, cm, delta);
     }
+
+    @Test
+    public void canConvertMeterToKilometer() {
+        double km = LengthConverter.meterToKilometer(5000);
+        assertEquals(5, km, delta);
+    }
+
+    @Test
+    public void canConvertMeterToMillimeter() {
+        double mm = LengthConverter.meterToMillimeter(2);
+        assertEquals(2000, mm, delta);
+    }
 }
