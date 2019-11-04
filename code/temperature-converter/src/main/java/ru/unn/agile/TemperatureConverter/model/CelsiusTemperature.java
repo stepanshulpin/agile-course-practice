bfd27b226d66acd0640a4ce3e0e1e0edba4520af
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-11-04 19:17:01 +0300

    Method order according to Java Code Conventions.

--- code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/CelsiusTemperature.java
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/CelsiusTemperature.java
@@ -1,17 +1,17 @@
 package ru.unn.agile.TemperatureConverter.model;
 
 public class CelsiusTemperature extends Temperature {
-    @Override
-    protected double getAbsoluteZero() {
-        final double absoluteZero = -273.15;
-        return absoluteZero;
-    }
-
     public CelsiusTemperature(final double value) throws IllegalArgumentException {
         super(value);
     }
 
     public CelsiusTemperature(final String value) {
         super(value);
+    }
+
+    @Override
+    protected double getAbsoluteZero() {
+        final double absoluteZero = -273.15;
+        return absoluteZero;
     }
 }
--- code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/FahrenheitTemperature.java
+++ code/temperature-converter/src/main/java/ru/unn/agile/TemperatureConverter/model/FahrenheitTemperature.java
@@ -1,17 +1,17 @@
 package ru.unn.agile.TemperatureConverter.model;
 
 public class FahrenheitTemperature extends Temperature {
-    @Override
-    protected double getAbsoluteZero() {
-        final double absoluteZero = -459.67;
-        return absoluteZero;
-    }
-
     public FahrenheitTemperature(final double value) throws IllegalArgumentException {
         super(value);
     }
 
     public FahrenheitTemperature(final String value) {
         super(value);
+    }
+
+    @Override
+    protected double getAbsoluteZero() {
+        final double absoluteZero = -459.67;
+        return absoluteZero;
     }
 }
