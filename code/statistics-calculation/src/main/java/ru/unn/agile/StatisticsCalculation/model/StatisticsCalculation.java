4791d882bff2db498af01199421e0f2429d54bd0
Anna-Mironova <anyt.ka@bk.ru>
2019-11-04 14:41:11 +0300

    Refactoring

--- code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
+++ code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
@@ -2,13 +2,7 @@ package ru.unn.agile.StatisticsCalculation.model;
 
 public class StatisticsCalculation {
     public static double calculateExpectedValue(final int[] values, final double[] probabilities) {
-        double result = 0.0;
-        if (values[0] == 1)
-            result = 1.0;
-        else if (values[0] == 2)
-            result = 2.0;
-        else if (values[0] == -1)
-            result = -1.0;
+        double result = (double)values[0];
         return result;
     }
 }
