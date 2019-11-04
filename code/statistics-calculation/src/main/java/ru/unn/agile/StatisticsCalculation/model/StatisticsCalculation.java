da0689e9025b2491b4d144534ed935affeb2466d
Anna-Mironova <anyt.ka@bk.ru>
2019-11-04 16:49:21 +0300

    Refactoring

--- code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
+++ code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/StatisticsCalculation.java
@@ -8,6 +8,9 @@ public class StatisticsCalculation {
         if (array.length == 0) {
             throw new IllegalArgumentException("Array should be initialized!");
         }
+        for (int i = 0; i< array.length;i++){
+            if (array[i] == null) throw new IllegalArgumentException("Array should be initialized!");
+        }
     }
 
     private static void checkArraysSize(final Number[] values, final Double[] probabilities){
@@ -22,8 +25,6 @@ public class StatisticsCalculation {
         checkArraysSize(values, probabilities);
         Double result = 0.0;
         for (int i = 0; i < values.length; i++) {
-            if (values[i] == null) throw new IllegalArgumentException("Array should be initialized!");
-            if (probabilities[i] == null) throw new IllegalArgumentException("Array should be initialized!");
             result += values[i].doubleValue() * probabilities[i];
         }
         return result;
