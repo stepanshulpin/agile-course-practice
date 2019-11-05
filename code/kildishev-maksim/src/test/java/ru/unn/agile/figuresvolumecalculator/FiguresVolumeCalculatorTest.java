cb50cc1e4140995094194c88e31a270422e1c0db
Maksim Kildishev <thanatormax@gmail.com>
2019-11-05 23:50:00 +0300

    Exception handling in tests refactored

--- code/kildishev-maksim/src/test/java/ru/unn/agile/figuresvolumecalculator/FiguresVolumeCalculatorTest.java
+++ code/kildishev-maksim/src/test/java/ru/unn/agile/figuresvolumecalculator/FiguresVolumeCalculatorTest.java
@@ -1,8 +1,7 @@
 package ru.unn.agile.figuresvolumecalculator;
 
-import org.junit.Rule;
+
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import ru.unn.agile.figuresvolumecalculator.model.FiguresVolumeCalculator;
 
 import static org.junit.Assert.*;
@@ -12,18 +11,13 @@ public class FiguresVolumeCalculatorTest {
 
     private final double delta = 1e-2;
 
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
     @Test
     public void canCalculatePyramidVolume() {
         assertEquals(3, FiguresVolumeCalculator.pyramidVolumeCalculate(3, 3), delta);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void canThrowExceptionIfSquareIsNegative() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Square can't be negative");
         FiguresVolumeCalculator.pyramidVolumeCalculate(-3, 3);
     }
 
@@ -37,10 +31,8 @@ public class FiguresVolumeCalculatorTest {
         assertEquals(113.097, FiguresVolumeCalculator.sphereVolumeCalculate(3), delta);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void canThrowExceptionIfSphereRadiusIsNegative() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Radius can't be negative");
         FiguresVolumeCalculator.sphereVolumeCalculate(-3);
     }
 
@@ -56,16 +48,14 @@ public class FiguresVolumeCalculatorTest {
                 FiguresVolumeCalculator.cylinderVolumeCalculate(3, -3), delta);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void canThrowExceptionIfCylinderBaseRadiusIsNegative() {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("Radius can't be negative");
         FiguresVolumeCalculator.cylinderVolumeCalculate(-3, 3);
     }
 
     @Test
     public void canCalculateConeVolumeWithZeroHeight() {
-        assertEquals(0, FiguresVolumeCalculator.coneVolumeCalculate(1,0), delta);
+        assertEquals(0, FiguresVolumeCalculator.coneVolumeCalculate(1, 0), delta);
     }
 
 
