20b85476263aa672e87cb620d97d9e0da81c4b8e
Anna-Mironova <anyt.ka@bk.ru>
2019-11-05 18:26:08 +0300

    Refactoring

--- code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/MomentOrderChecker.java
+++ code/statistics-calculation/src/main/java/ru/unn/agile/StatisticsCalculation/model/MomentOrderChecker.java
@@ -2,11 +2,19 @@ package ru.unn.agile.StatisticsCalculation.model;
 
 public class MomentOrderChecker {
     public static void checkOrder(final Integer order) {
-        if (order == null) {
-            throw new IllegalArgumentException("Raw moment order should not be null!");
-        }
+        checkNegativeOrder(order);
+        checkNullOrder(order);
+    }
+
+    private static void checkNegativeOrder(final Integer order) {
         if (order <= 0) {
             throw new IllegalArgumentException("Raw moment order should be more than 0");
+        }
+    }
+
+    private static void checkNullOrder(final Integer order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Raw moment order should not be null!");
         }
     }
 
