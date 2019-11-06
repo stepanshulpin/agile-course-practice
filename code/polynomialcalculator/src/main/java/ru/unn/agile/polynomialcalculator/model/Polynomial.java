0d9684d5664103bde164ac3f62cc73d842294d17
vgvoleg <vgvoleg@gmail.com>
2019-11-07 00:05:58 +0300

    refactor coefs

--- code/polynomialcalculator/src/main/java/ru/unn/agile/polynomialcalculator/model/Polynomial.java
+++ code/polynomialcalculator/src/main/java/ru/unn/agile/polynomialcalculator/model/Polynomial.java
@@ -1,10 +1,16 @@
 package ru.unn.agile.polynomialcalculator.model;
 
+import java.util.ArrayList;
+
 public class Polynomial {
 
     private int degree = 0;
+    private ArrayList<Double> coefs;
 
-    public Polynomial() {}
+    public Polynomial() {
+        this.coefs = new ArrayList<>();
+        this.coefs.add(0.);
+    }
 
     public int getDegree() {
         return degree;
