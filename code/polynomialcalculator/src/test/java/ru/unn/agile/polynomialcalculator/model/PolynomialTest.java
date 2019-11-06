2e4027f8daa8aeff26ed2d7715c461708640e254
vgvoleg <vgvoleg@gmail.com>
2019-11-06 23:50:38 +0300

    default constructor and test

--- code/polynomialcalculator/src/test/java/ru/unn/agile/polynomialcalculator/model/PolynomialTest.java
+++ code/polynomialcalculator/src/test/java/ru/unn/agile/polynomialcalculator/model/PolynomialTest.java
@@ -5,4 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PolynomialTest {
+    @Test
+    public void canInitDefaultPolynomial() {
+        var polynomial = new Polynomial();
+        assertNotNull(polynomial);
+    }
 }
