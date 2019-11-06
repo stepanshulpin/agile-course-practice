3ce163e58b8734c341d94f16bc2cc8c031f85ba8
Kirill Iuferev <juferev18@gmail.com>
2019-11-06 19:27:02 +0300

    Edited tests (local variables added)

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/IuferevKirillTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/IuferevKirillTest.java
@@ -11,24 +11,28 @@ public class IuferevKirillTest {
     @Test
     public void areNumberIsNotEqualComplexNumber() {
         ComplexNumber complexNumber = new ComplexNumber(2.4, 4.4);
-        assertFalse(complexNumber.equals(24));
+        var numberValueToCompare = 24;
+        assertFalse(complexNumber.equals(numberValueToCompare));
     }
 
     @Test
     public void canFormatToString() {
         ComplexNumber complexNumber = new ComplexNumber(2.4, 4.4);
-        assertEquals("2.4 + 4.4i", complexNumber.toString());
+        var expectedString = "2.4 + 4.4i";
+        assertEquals(expectedString, complexNumber.toString());
     }
 
     @Test
     public void canGetImaginaryPart() {
         ComplexNumber complexNumber = new ComplexNumber(2.4, 4.4);
-        assertTrue(complexNumber.getImaginary() == 4.4);
+        var expectedImaginaryPart = 4.4;
+        assertTrue(complexNumber.getImaginary() == expectedImaginaryPart);
     }
 
     @Test
     public void canGetRealPart() {
         ComplexNumber complexNumber = new ComplexNumber(2.4, 4.4);
-        assertEquals(2.4, complexNumber.getReal(), delta);
+        var expectedRealPart = 2.4;
+        assertEquals(expectedRealPart, complexNumber.getReal(), delta);
     }
 }
