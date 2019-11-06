c1a912f6e4b4d95cc9e7c074992857e1cfafe916
vgvoleg <vgvoleg@gmail.com>
2019-11-06 23:53:49 +0300

    refactor degree

--- code/polynomialcalculator/src/main/java/ru/unn/agile/polynomialcalculator/model/Polynomial.java
+++ code/polynomialcalculator/src/main/java/ru/unn/agile/polynomialcalculator/model/Polynomial.java
@@ -2,9 +2,11 @@ package ru.unn.agile.polynomialcalculator.model;
 
 public class Polynomial {
 
+    private int degree = 0;
+
     public Polynomial() {}
 
     public int getDegree() {
-        return 0;
+        return degree;
     }
 }
