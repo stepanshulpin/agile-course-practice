5a2196141726a2df4127a24989f4f0cf72bbdee9
Mitrokhin <MitrokhinNP@gmail.ru>
2019-11-06 11:11:01 +0300

    Added more unit tests

--- code/queue/src/test/java/ru/unn/agile/Queue/Model/QueueTest.java
+++ code/queue/src/test/java/ru/unn/agile/Queue/Model/QueueTest.java
@@ -23,4 +23,65 @@ public class QueueTest {
 
         assertEquals(expectedElement, newQueue.getTail());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwWhenPushNullElementToQueue() {
+        Queue<Integer> newQueue = new Queue<>();
+
+        newQueue.push(null);
+    }
+
+    @Test
+    public void canPushDoublerElementToQueue() {
+        Queue<Double> newQueue = new Queue<>();
+        Double expectedElement = 1.0;
+
+        newQueue.push(expectedElement);
+
+        assertEquals(expectedElement, newQueue.getTail());
+    }
+
+    @Test
+    public void isQueueWithoutIntegerElementsEmptiness() {
+        Queue<Integer> newQueue = new Queue<>();
+
+        assertTrue(newQueue.isEmpty());
+    }
+
+    @Test
+    public void isQueueWithoutDoubleElementsEmptiness() {
+        Queue<Double> newQueue = new Queue<>();
+
+        assertTrue(newQueue.isEmpty());
+    }
+
+    @Test
+    public void isQueueWithOneIntegerElementNotEmpty() {
+        Queue<Integer> newQueue = new Queue<>();
+        newQueue.push(1);
+
+        assertFalse(newQueue.isEmpty());
+    }
+
+    @Test
+    public void isQueueWithOneDoubleElementNotEmpty() {
+        Queue<Double> newQueue = new Queue<>();
+        newQueue.push(1.0);
+
+        assertFalse(newQueue.isEmpty());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwWhenGetTailIntegerElementFromEmptyQueue() {
+        Queue<Integer> newQueue = new Queue<>();
+
+        newQueue.getTail();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwWhenGetTailDoubleElementFromEmptyQueue() {
+        Queue<Double> newQueue = new Queue<>();
+
+        newQueue.getTail();
+    }
 }
