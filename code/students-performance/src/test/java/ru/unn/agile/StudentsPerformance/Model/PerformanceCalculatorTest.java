a6edbdabd2b9053ae7e431c7af176bdc5ea2f661
Vaginak Simonyan <vaginak.simonyan.consultant@nielsen.com>
2019-11-06 16:54:08 +0300

    Fixed Travis issues

--- code/students-performance/src/test/java/ru/unn/agile/StudentsPerformance/Model/PerformanceCalculatorTest.java
+++ code/students-performance/src/test/java/ru/unn/agile/StudentsPerformance/Model/PerformanceCalculatorTest.java
@@ -13,12 +13,7 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canCreateCalculator() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5)))
-                        )
-                );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
         assertNotNull(calculator);
     }
@@ -37,28 +32,20 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetExcellentStudentsNumberIfExist() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
-                )
-        );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
-        assertEquals(2, calculator.getExcellentStudentsNumber(), compareDelta);
+        assertEquals(1, calculator.getExcellentStudentsNumber(), compareDelta);
     }
 
     @Test
     public void canGetExcellentStudentsNumberIfNotExist() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 3, 5, 2, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 4, 5, 4, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
+                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 5, 4))),
+                        new Student(new ArrayList<>(List.of(5, 3, 5, 2, 5))),
+                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 3, 5))),
+                        new Student(new ArrayList<>(List.of(5, 4, 5))),
+                        new Student(new ArrayList<>(List.of(3, 4, 4, 2, 4, 3)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -67,15 +54,7 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetGoodStudentsNumberIfExist() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(4, 4, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
-                )
-        );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
         assertEquals(1, calculator.getGoodStudentsNumber(), compareDelta);
     }
@@ -84,11 +63,11 @@ public class PerformanceCalculatorTest {
     public void canGetGoodStudentsNumberIfNotExist() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 3, 5, 2, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
+                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 2, 4))),
+                        new Student(new ArrayList<>(List.of(5, 3, 5, 2, 4, 5))),
+                        new Student(new ArrayList<>(List.of(5, 2, 4, 5, 2, 5))),
+                        new Student(new ArrayList<>(List.of(5, 5, 5, 5))),
+                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 5, 3)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -97,28 +76,20 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetSatisfactoryStudentsNumberIfExist() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(4, 4, 5, 5, 3, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
-                )
-        );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
-        assertEquals(2, calculator.getSatisfactoryStudentsNumber(), compareDelta);
+        assertEquals(1, calculator.getSatisfactoryStudentsNumber(), compareDelta);
     }
 
     @Test
     public void canGetSatisfactoryStudentsNumberIfNotExist() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
+                        new Student(new ArrayList<>(List.of(5, 5, 2, 5, 4, 4))),
                         new Student(new ArrayList<>(List.of(5, 3, 5, 2, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 4, 4, 4, 4)))
+                        new Student(new ArrayList<>(List.of(3, 2, 4, 2, 2, 5))),
+                        new Student(new ArrayList<>(List.of(5, 5, 2, 5, 5, 2))),
+                        new Student(new ArrayList<>(List.of(5, 3, 4, 4, 2, 4)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -127,28 +98,20 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetBadStudentsNumberIfExist() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(4, 4, 5, 5, 3, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 2, 4, 4, 3)))
-                )
-        );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
-        assertEquals(3, calculator.getBadStudentsNumber(), compareDelta);
+        assertEquals(2, calculator.getBadStudentsNumber(), compareDelta);
     }
 
     @Test
     public void canGetBadStudentsNumberIfNotExist() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 3, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 3, 5, 4, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 4, 4, 5, 3, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 4, 4, 4, 4)))
+                        new Student(new ArrayList<>(List.of(5, 3, 3, 4, 4, 5))),
+                        new Student(new ArrayList<>(List.of(5, 3, 5, 4, 4, 5))),
+                        new Student(new ArrayList<>(List.of(3, 4, 5, 5, 3, 5))),
+                        new Student(new ArrayList<>(List.of(5, 3, 5, 5, 5, 5))),
+                        new Student(new ArrayList<>(List.of(5, 5, 4, 5, 4, 4)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -157,30 +120,14 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetStudentsAverageRating() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
-                )
-        );
+        List<Student> students = generateDefaultStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
-        assertEquals(4.233, calculator.getAverageRating(), compareDelta);
+        assertEquals(4.2, calculator.getAverageRating(), compareDelta);
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void canGetStudentsAverageRatingIfAllStudentsAreEmpty() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(),
-                        new Student(),
-                        new Student(),
-                        new Student(),
-                        new Student()
-                )
-        );
+        List<Student> students = generateEmptyStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
         calculator.getAverageRating();
     }
@@ -189,11 +136,11 @@ public class PerformanceCalculatorTest {
     public void canGetStudentsAverageRatingIfIsEmptyStudent() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
+                        new Student(new ArrayList<>(List.of(4, 4, 2, 5, 3, 5))),
                         new Student(),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
+                        new Student(new ArrayList<>(List.of(5, 2, 2, 5, 4, 3))),
+                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5))),
+                        new Student(new ArrayList<>(List.of(4, 4, 4, 5, 3, 3)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -204,11 +151,11 @@ public class PerformanceCalculatorTest {
     public void canGetTotalMarksNumber() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4))),
+                        new Student(new ArrayList<>(List.of(3, 3, 4, 2, 4))),
                         new Student(),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5, 4))),
-                        new Student(new ArrayList<>(List.of(3, 5, 4)))
+                        new Student(new ArrayList<>(List.of(3, 2, 4, 4, 2, 4))),
+                        new Student(new ArrayList<>(List.of(5, 5, 2, 5, 5, 3, 4))),
+                        new Student(new ArrayList<>(List.of(3, 5, 5)))
                 )
         );
         PerformanceCalculator calculator = new PerformanceCalculator(students);
@@ -217,15 +164,7 @@ public class PerformanceCalculatorTest {
 
     @Test
     public void canGetTotalMarksNumberIfNoMarks() {
-        List<Student> students = new ArrayList<>(
-                List.of(
-                        new Student(),
-                        new Student(),
-                        new Student(),
-                        new Student(),
-                        new Student()
-                )
-        );
+        List<Student> students = generateEmptyStudentsList();
         PerformanceCalculator calculator = new PerformanceCalculator(students);
         assertEquals(0, calculator.getTotalMarksNumber(), compareDelta);
     }
@@ -234,10 +173,10 @@ public class PerformanceCalculatorTest {
     public void canGetPercentageOfTraining() {
         List<Student> students = new ArrayList<>(
                 List.of(
-                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4))),
+                        new Student(new ArrayList<>(List.of(5, 3, 5, 2, 4))),
                         new Student(),
-                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
-                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5, 4))),
+                        new Student(new ArrayList<>(List.of(5, 2, 2, 3, 4, 5))),
+                        new Student(new ArrayList<>(List.of(5, 5, 5, 4, 5, 5, 4))),
                         new Student(new ArrayList<>(List.of(5, 5))),
                         new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5, 4, 2, 3, 5, 2))),
                         new Student(new ArrayList<>(List.of(3, 5, 4)))
@@ -249,7 +188,25 @@ public class PerformanceCalculatorTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void canGetPercentageOfTrainingIfNoMarks() {
-        List<Student> students = new ArrayList<>(
+        List<Student> students = generateEmptyStudentsList();
+        PerformanceCalculator calculator = new PerformanceCalculator(students);
+        calculator.getPercentageOfTraining();
+    }
+
+    private List<Student> generateDefaultStudentsList() {
+        return new ArrayList<>(
+                List.of(
+                        new Student(new ArrayList<>(List.of(5, 3, 2, 5, 4, 4))),
+                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 5))),
+                        new Student(new ArrayList<>(List.of(3, 2, 4, 5, 2, 5))),
+                        new Student(new ArrayList<>(List.of(5, 5, 5, 5, 5, 4))),
+                        new Student(new ArrayList<>(List.of(3, 5, 4, 4, 4, 3)))
+                )
+        );
+    }
+
+    private List<Student> generateEmptyStudentsList() {
+        return new ArrayList<>(
                 List.of(
                         new Student(),
                         new Student(),
@@ -258,7 +215,5 @@ public class PerformanceCalculatorTest {
                         new Student()
                 )
         );
-        PerformanceCalculator calculator = new PerformanceCalculator(students);
-        calculator.getPercentageOfTraining();
     }
 }
