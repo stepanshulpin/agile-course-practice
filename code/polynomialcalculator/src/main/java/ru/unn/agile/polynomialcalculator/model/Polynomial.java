513eeae74285e6acdb75133a62a850811846719a
vgvoleg <vgvoleg@gmail.com>
2019-11-06 23:42:12 +0300

    initial commit of polynomial calculator

---
+++ code/polynomialcalculator/src/main/java/ru/unn/agile/polynomialcalculator/model/Polynomial.java
@@ -0,0 +1,7 @@
+package ru.unn.agile.polynomialcalculator.model;
+
+public class Polynomial {
+
+    public Polynomial() {}
+
+}
---
+++ code/polynomialcalculator/src/test/java/ru/unn/agile/polynomialcalculator/model/PolynomialTest.java
@@ -0,0 +1,8 @@
+package ru.unn.agile.polynomialcalculator.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PolynomialTest {
+}
