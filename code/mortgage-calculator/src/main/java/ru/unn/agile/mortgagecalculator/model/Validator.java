3a24862ef20b0470d541970c85a50c84cc4db90e
stepanshulpin <steve13.12@yandex.ru>
2019-11-07 23:04:45 +0300

    validator refactoring

--- code/mortgage-calculator/src/main/java/ru/unn/agile/mortgagecalculator/model/Validator.java
+++ code/mortgage-calculator/src/main/java/ru/unn/agile/mortgagecalculator/model/Validator.java
@@ -1,9 +1,9 @@
 package ru.unn.agile.mortgagecalculator.model;
 
-public class Validator {
+class Validator {
 
-    public void checkPositiveInteger(int value) {
-        if(value <= 0) {
+    void checkPositiveInteger(int value) {
+        if (value <= 0) {
             throw new IllegalArgumentException("A positive integer is expected");
         }
     }
