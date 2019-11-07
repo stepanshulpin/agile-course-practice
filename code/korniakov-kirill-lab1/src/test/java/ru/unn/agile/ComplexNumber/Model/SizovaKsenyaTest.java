bfd7bc2e9ba8e46ac7cd5a99d35d6b20cb5d86e2
Ksenya Sizova <sizova.ksuniy@mail.ru>
2019-11-07 21:52:23 +0300

    Minor fix

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/SizovaKsenyaTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/SizovaKsenyaTest.java
@@ -11,28 +11,34 @@ public class SizovaKsenyaTest {
     @Test
     public void canCreateDefaultComplexNumber() {
         ComplexNumber complexNumber = new ComplexNumber();
+        double expectedRealPart = 0;
+        double expectedImagPart = 0;
 
         // Assert
-        assertEquals(complexNumber.getReal(), 0, delta);
-        assertEquals(complexNumber.getImaginary(), 0, delta);
+        assertEquals(complexNumber.getReal(), expectedRealPart, delta);
+        assertEquals(complexNumber.getImaginary(), expectedImagPart, delta);
     }
 
     @Test
     public void canCreateComplexNumberWithValues() {
         ComplexNumber complexNumber = new ComplexNumber(1, 2);
+        double expectedRealPart = 1;
+        double expectedImagPart = 2;
 
         // Assert
-        assertEquals(complexNumber.getReal(), 1, delta);
-        assertEquals(complexNumber.getImaginary(), 2, delta);
+        assertEquals(complexNumber.getReal(), expectedRealPart, delta);
+        assertEquals(complexNumber.getImaginary(), expectedImagPart, delta);
     }
 
     @Test
     public void canCreateComplexNumberWithString() {
         ComplexNumber complexNumber = new ComplexNumber("1", "2");
+        double expectedRealPart = 1;
+        double expectedImagPart = 2;
 
         // Assert
-        assertEquals(complexNumber.getReal(), 1, delta);
-        assertEquals(complexNumber.getImaginary(), 2, delta);
+        assertEquals(complexNumber.getReal(), expectedRealPart, delta);
+        assertEquals(complexNumber.getImaginary(), expectedImagPart, delta);
     }
 
     @Test
@@ -41,10 +47,12 @@ public class SizovaKsenyaTest {
         ComplexNumber complexNumber2 = new ComplexNumber(2.5, 1.5);
 
         ComplexNumber complexNumber3 = complexNumber1.add(complexNumber2);
+        double expectedRealPart = 4;
+        double expectedImagPart = 4;
 
         // Assert
-        assertEquals(complexNumber3.getReal(), 4, delta);
-        assertEquals(complexNumber3.getImaginary(), 4, delta);
+        assertEquals(complexNumber3.getReal(), expectedRealPart, delta);
+        assertEquals(complexNumber3.getImaginary(), expectedImagPart, delta);
     }
 
     @Test
@@ -61,9 +69,10 @@ public class SizovaKsenyaTest {
         ComplexNumber complexNumber1 = new ComplexNumber(2, 10);
         ComplexNumber complexNumber2 = new ComplexNumber(-5, 2);
         ComplexNumber complexNumber3 = complexNumber1.add(complexNumber2);
+        String expectedStringView = "-3.0 + 12.0i";
 
         // Assert
-        assertEquals("-3.0 + 12.0i", complexNumber3.toString());
+        assertEquals(expectedStringView, complexNumber3.toString());
     }
 
     @Test
