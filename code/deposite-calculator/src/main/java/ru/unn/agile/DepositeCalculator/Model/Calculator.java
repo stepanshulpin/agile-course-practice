9d853405379d6921f7bb7d7680454035134f296f
VitKonevskiy <konevskiy.v@gmail.com>
2019-11-07 08:05:51 +0300

    calc deposit for year without capitalization

--- code/deposite-calculator/src/main/java/ru/unn/agile/DepositeCalculator/Model/Calculator.java
+++ code/deposite-calculator/src/main/java/ru/unn/agile/DepositeCalculator/Model/Calculator.java
@@ -2,4 +2,9 @@ package ru.unn.agile.DepositeCalculator.Model;
 
 public class Calculator {
 
+    private final int MAX_PERCENT = 100;
+
+    public double calculate(double startSum, int percent) {
+        return startSum + startSum * percent / MAX_PERCENT;
+    }
 }
--- code/deposite-calculator/src/test/java/ru/unn/agile/DepositeCalculator/Model/CalculatorTest.java
+++ code/deposite-calculator/src/test/java/ru/unn/agile/DepositeCalculator/Model/CalculatorTest.java
@@ -5,4 +5,13 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    private final double delta = 0.0001;
+
+    @Test
+    public void caclYearDepositeWithoutCapitalization() {
+        Calculator calc = new Calculator();
+        double depositeRes = calc.calculate(100, 10);
+
+        assertEquals(110, depositeRes, delta);
+    }
 }
