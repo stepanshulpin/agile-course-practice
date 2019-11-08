2bbf8f495c8db8af7edc0c027c2fa60493c43cba
Viol <Folifolo@yandex.ru>
2019-11-08 18:14:10 +0300

    add tests and refactor a little

--- code/gameoflife/src/main/java/ru/unn/agile/gameoflife/model/GameOfLife.java
+++ code/gameoflife/src/main/java/ru/unn/agile/gameoflife/model/GameOfLife.java
@@ -1,58 +1,77 @@
 package ru.unn.agile.GameOfLife.model;
 
 public class GameOfLife {
-    private char[][] Grid;
+    private char[][] grid;
     private int height;
     private int width;
 
-    public GameOfLife(int height, int width){
-        this.height = height;
-        this.width = width;
+    public GameOfLife(int height, int width) {
+        if ((height < 0) && (width < 0)) {
+            this.height = 0;
+            this.width = 0;
+        }
+        else {
+            this.height = height;
+            this.width = width;
+        }
 
-        Grid = new char[this.height][this.width];
+        grid = new char[this.height][this.width];
+
+        for (int i = 0; i < this.height; i++)
+            for (int j = 0; j < this.width; j++)
+                grid[i][j] = '.';
+    }
+
+    public GameOfLife(String str, int height, int width) {
+        this(height, width);
 
         for(int i = 0; i <this.height; i++)
             for(int j = 0; j <this.width; j++)
-                Grid[i][j] = '.';
+                grid[i][j] = str.charAt(i*this.width+j);
     }
 
-    public boolean IsPointOnGrid(int y, int x){
+    public boolean isPointOnGrid(int y, int x) {
         if((y >= 0) && (y < this.width) && (x >= 0) && (x < this.height))
             return true;
         else
             return false;
     }
 
-    public void SetCell(int y, int x){
-        if(IsPointOnGrid(y,x))
-            Grid[y][x] = '*';
+    public void setCell(int y, int x) {
+        if(isPointOnGrid(y, x))
+            grid[y][x] = '*';
     }
 
-    public void DeleteCell(int y, int x){
-        Grid[y][x] = '.';
+    public void deleteCell(int y, int x) {
+        grid[y][x] = '.';
     }
-    public boolean IsCellLive(int y, int x) {
-        if(IsPointOnGrid(y,x))
-            return Grid[y][x]=='*';
+    public boolean isCellLive(int y, int x) {
+        if(isPointOnGrid(y, x))
+            return grid[y][x] == '*';
         else
             return false;
     }
 
-    public int CheckNeighborsNum(int y, int x){
+    public int getNeighborsNum(int y, int x) {
         int NeighborsNum = 0;
         for(int i = y-1; i <= y+1; i++)
             for(int j = x-1; j <= x+1; j++)
                     if ((i != 0) || (j != 0)) {
-                        NeighborsNum += this.IsCellLive(i, j) ? 1 : 0;
+                        NeighborsNum += this.isCellLive(i, j) ? 1 : 0;
                     }
         return NeighborsNum;
     }
 
-    public void MakeTurn(){
-        for(int i = 0; i <this.height; i++)
-            for(int j = 0; j <this.width; j++)
-                if (Grid[i][j] == '*')
-                    Grid[i][j] = '.';
+    public void makeTurn() {
+        for(int i = 0; i < this.height; i++)
+            for(int j = 0; j < this.width; j++) {
+                if ((getNeighborsNum(i, j) < 2) || (getNeighborsNum(i, j) > 3))
+                    deleteCell(i, j);
+                if (getNeighborsNum(i, j) == 3)
+                    setCell(i,j);
+            }
     }
+
 }
+
 
--- code/gameoflife/src/test/java/ru/unn/agile/GameOfLife/Model/GameOfLifeTest.java
+++ code/gameoflife/src/test/java/ru/unn/agile/GameOfLife/Model/GameOfLifeTest.java
@@ -9,15 +9,15 @@ public class GameOfLifeTest {
     public void canCreateGrid() {
         GameOfLife g = new GameOfLife(3,3);
 
-        assertNotNull(g.IsCellLive(1,1));
+        assertNotNull(g.isCellLive(1,1));
     }
 
     @Test
     public void canSetCell(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(1,1);
+        g.setCell(1,1);
 
-        assertTrue(g.IsCellLive(1,1));
+        assertTrue(g.isCellLive(1,1));
     }
 
     @Test
@@ -27,77 +27,143 @@ public class GameOfLifeTest {
 
         for(int i = 0; i <size; i++)
             for(int j = 0; j <size; j++)
-                assertFalse(g.IsCellLive(1,1));
+                assertFalse(g.isCellLive(1,1));
     }
 
     @Test
     public void canDeleteCell(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(1,1);
+        g.setCell(1,1);
 
-        g.DeleteCell(1,1);
-        assertFalse(g.IsCellLive(1,1));
+        g.deleteCell(1,1);
+        assertFalse(g.isCellLive(1,1));
     }
 
     @Test
     public void canMakeTurn(){
         GameOfLife g = new GameOfLife(3,3);
 
-        g.MakeTurn();
+        g.makeTurn();
 
-        assertFalse(g.IsCellLive(1,1));
+        assertFalse(g.isCellLive(1,1));
     }
 
     @Test
     public void isOneLivingCellDies(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(1,1);
+        g.setCell(1,1);
 
-        g.MakeTurn();
+        g.makeTurn();
 
-        assertFalse(g.IsCellLive(1,1));
+        assertFalse(g.isCellLive(1,1));
     }
 
     @Test
     public void isNeighborsNumCorrect(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(0,1);
-        g.SetCell(1,0);
+        g.setCell(0,1);
+        g.setCell(1,0);
 
-        assertEquals(2, g.CheckNeighborsNum(1,1));
+        assertEquals(2, g.getNeighborsNum(1,1));
     }
 
     @Test
     public void isNeighborsNumCorrectOnLeftBorder(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(0,1);
-        g.SetCell(1,0);
+        g.setCell(0,1);
+        g.setCell(1,0);
 
-        assertEquals(2, g.CheckNeighborsNum(0,0));
+        assertEquals(2, g.getNeighborsNum(0,0));
     }
 
     @Test
     public void isNeighborsNumCorrectOnRightBorder(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(1,1);
-        g.SetCell(1,2);
+        g.setCell(1,1);
+        g.setCell(1,2);
 
-        assertEquals(2, g.CheckNeighborsNum(2,2));
+        assertEquals(2, g.getNeighborsNum(2,2));
     }
 
     @Test
     public void canGetCellBeyondBorder(){
         GameOfLife g = new GameOfLife(3,3);
 
-        assertFalse(g.IsCellLive(4,4));
+        assertFalse(g.isCellLive(4,4));
     }
 
     @Test
     public void canSetCellBeyondBorder(){
         GameOfLife g = new GameOfLife(3,3);
-        g.SetCell(4,4);
+        g.setCell(4,4);
 
-        assertFalse(g.IsCellLive(4,4));
+        assertFalse(g.isCellLive(4,4));
+    }
+
+    @Test
+    public void canCreateNegativeGrid(){
+        GameOfLife g = new GameOfLife(-3,-3);
+
+        assertNotNull(g);
+    }
+
+    @Test
+    public void isCellWithTwoNeighboursLive(){
+        GameOfLife g = new GameOfLife(3,3);
+        g.setCell(1,1); // .*.
+        g.setCell(1,0); // **.
+        g.setCell(0,1); // ...
+
+        g.makeTurn();
+
+        assertTrue(g.isCellLive(1,1));
+    }
+    @Test
+    public void isCellWithThreeNeighboursLive(){
+        GameOfLife g = new GameOfLife(3,3);
+        g.setCell(0,0); // **.
+        g.setCell(0,1); // **.
+        g.setCell(1,0); // ...
+        g.setCell(1,1);
+
+
+        g.makeTurn();
+
+        assertTrue(g.isCellLive(0,0));
+    }
+    @Test
+    public void isCellWithThreeNeighboursRevive(){
+        GameOfLife g = new GameOfLife(3,3);
+        g.setCell(0,0); // **.
+        g.setCell(0,1); // *..
+        g.setCell(1,0); // ...
+
+        g.makeTurn();
+
+        assertTrue(g.isCellLive(0,0));
+    }
+
+    @Test
+    public void isCellWithFourNeighborsDies(){
+        GameOfLife g = new GameOfLife(3,3);
+        g.setCell(0,0); // **.
+        g.setCell(0,1); // ***
+        g.setCell(1,0); // ...
+        g.setCell(1,1);
+        g.setCell(1,2);
+
+        g.makeTurn();
+
+        assertFalse(g.isCellLive(1,1));
+    }
+
+    @Test
+    public void canCreateFromString(){
+        GameOfLife g = new GameOfLife("..." +
+                                      ".*." +
+                                      "...", 3, 3);
+
+        assertTrue(g.isCellLive(1,1));
     }
 
 }
