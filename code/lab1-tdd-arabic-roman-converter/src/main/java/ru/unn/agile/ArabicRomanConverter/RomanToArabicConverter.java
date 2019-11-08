ad382a250563175180b001f2259fa4346a5bdae8
ViolettaShirokikh <vaylet96@mail.ru>
2019-11-08 15:32:11 +0300

    Fixed Travis issues

--- code/lab1-tdd-arabic-roman-converter/src/main/java/ru/unn/agile/ArabicRomanConverter/RomanToArabicConverter.java
+++ code/lab1-tdd-arabic-roman-converter/src/main/java/ru/unn/agile/ArabicRomanConverter/RomanToArabicConverter.java
@@ -4,17 +4,19 @@ import java.util.Hashtable;
 
 public class RomanToArabicConverter {
 
+    private static final int[] ARABIC_NUMERALS = {1, 5, 10, 50,
+            100, 500, 1000};
     private Hashtable<Character, Integer> ht;
 
     public RomanToArabicConverter() {
         ht = new Hashtable<>();
-        ht.put('I', 1);
-        ht.put('V', 5);
-        ht.put('X', 10);
-        ht.put('L', 50);
-        ht.put('C', 100);
-        ht.put('D', 500);
-        ht.put('M', 1000);
+        ht.put('I', ARABIC_NUMERALS[0]);
+        ht.put('V', ARABIC_NUMERALS[1]);
+        ht.put('X', ARABIC_NUMERALS[2]);
+        ht.put('L', ARABIC_NUMERALS[3]);
+        ht.put('C', ARABIC_NUMERALS[4]);
+        ht.put('D', ARABIC_NUMERALS[5]);
+        ht.put('M', ARABIC_NUMERALS[6]);
     }
 
     public int convert(final String romanNumber) {
