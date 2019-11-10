f88affe93d59b2c484fc46c9a6cf4eafae7f4ddf
Lenferd <DeysonSH@gmail.com>
2019-11-10 15:57:51 +0300

    Add element

--- code/tree/src/main/java/ru/unn/agile/tree/model/Tree.java
+++ code/tree/src/main/java/ru/unn/agile/tree/model/Tree.java
@@ -1,4 +1,9 @@
 package ru.unn.agile.tree.model;
 
 public class Tree {
+    private double _value;
+
+    public void addElement(final double value) {
+        _value = value;
+    }
 }
--- code/tree/src/test/java/ru/unn/agile/tree/model/TreeTests.java
+++ code/tree/src/test/java/ru/unn/agile/tree/model/TreeTests.java
@@ -4,4 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TreeTests {
+    @Test
+    public void canCreate() {
+        Tree tree = new Tree();
+
+        assertNotNull(tree);
+    }
+
+    @Test
+    public void canAddElement() {
+        Tree tree = new Tree();
+        final float number = 0;
+
+        tree.addElement(number);
+    }
 }
