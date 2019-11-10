06e3ca55cc7406d77f7b4e5b6b33c48247bbb9fc
Nesterov Alexander <nesoldr@mail.ru>
2019-11-11 01:56:58 +0300

    Init project - BitArray

---
+++ code/bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
@@ -0,0 +1,4 @@
+package ru.unn.agile.bitarray.model;
+
+public class BitArray {
+}
---
+++ code/bit-array/src/test/java/ru/unn/agile/bitarray/Model/BitArrayTest.java
@@ -0,0 +1,15 @@
+package ru.unn.agile.bitarray.Model;
+
+import org.junit.Test;
+import ru.unn.agile.bitarray.model.BitArray;
+
+import static org.junit.Assert.*;
+
+public class BitArrayTest {
+
+    @Test
+    public void check() {
+        BitArray btr;
+        assertEquals(0, 0);
+    }
+}
