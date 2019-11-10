8f328a71c8272b5324ba25b4d0b029ebcccde7a7
Alexander Nesterov <nesoldr@mail.ru>
2019-11-10 21:08:19 -0100

    Added test for test function getArg

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/NesterovAlexanderTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/NesterovAlexanderTest.java
@@ -38,6 +38,42 @@ public class NesterovAlexanderTest {
     }
 
     @Test
+    public void canArgNumberHalfPI() {
+        // Arrange
+        ComplexNumber z = new ComplexNumber(0, 1);
+
+        // Act
+        double res = getArg(z);
+
+        // Assert
+        assertEquals(res, Math.PI / 2, 1e-15);
+    }
+
+    @Test
+    public void canArgNumberZero() {
+        // Arrange
+        ComplexNumber z = new ComplexNumber(1, 0);
+
+        // Act
+        double res = getArg(z);
+
+        // Assert
+        assertEquals(res, 0, 1e-15);
+    }
+
+    @Test
+    public void canArgNumberQuarterPI() {
+        // Arrange
+        ComplexNumber z = new ComplexNumber(Math.sqrt(2) / 2, Math.sqrt(2) / 2);
+
+        // Act
+        double res = getArg(z);;
+
+        // Assert
+        assertEquals(res, Math.PI / 4, 1e-15);
+    }
+
+    @Test
     public void canWorkWithManyOperations() {
         // Arrange
         ComplexNumber z1 = new ComplexNumber(5.78, 3.87);
