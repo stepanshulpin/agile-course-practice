050a0c2703f7b9dae8bcfad649eef5eaa8d96361
Daria Kochetova <ko4.dasha@yandex.ru>
2019-11-12 21:17:34 +0300

    Iteration 2. Green test and Refactoring.

--- code/kochetova-dasha-lab1-matrix/src/main/java/ru/unn/agile/matrix/model/Matrix.java
+++ code/kochetova-dasha-lab1-matrix/src/main/java/ru/unn/agile/matrix/model/Matrix.java
@@ -1,9 +1,13 @@
 package ru.unn.agile.Matrix.model;
 
 public class Matrix {
-    private final double size;
+    private final int size;
 
     public Matrix(final int size) {
+        if (size == 0) {
+            throw new IllegalArgumentException("Trying to create a matrix with size zero");
+        }
+
         this.size = size;
     }
 }
--- code/kochetova-dasha-lab1-matrix/src/test/java/ru/unn/agile/Matrix/Model/MatrixTest.java
+++ code/kochetova-dasha-lab1-matrix/src/test/java/ru/unn/agile/Matrix/Model/MatrixTest.java
@@ -1,20 +1,28 @@
 package ru.unn.agile.Matrix.Model;
 
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import ru.unn.agile.Matrix.model.Matrix;
 
 import static org.junit.Assert.*;
 
+
 public class MatrixTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void canCreateSquareMatrix() {
         var squareMatrix = new Matrix(3);
         assertNotNull(squareMatrix);
     }
-    
+
     @Test
-    public void canCreateSquareMatrixWithZeroSize() {
+    public void cantCreateSquareMatrixWithZeroSize() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Trying to create a matrix with size zero");
+
         var squareMatrix = new Matrix(0);
-        assertNotNull(squareMatrix);
     }
 }
