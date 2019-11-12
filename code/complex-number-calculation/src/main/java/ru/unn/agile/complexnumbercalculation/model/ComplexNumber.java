6a87b538fddff712b95a926fa06869facaa8ce1e
Julia Kulygina <julia.kulygina@vicuesoft.com>
2019-11-12 18:21:07 +0300

    Add test for constructor with parameters and implementation

--- code/complex-number-calculation/src/main/java/ru/unn/agile/complexnumbercalculation/model/ComplexNumber.java
+++ code/complex-number-calculation/src/main/java/ru/unn/agile/complexnumbercalculation/model/ComplexNumber.java
@@ -3,10 +3,17 @@ package ru.unn.agile.complexnumbercalculation.model;
 public class ComplexNumber {
     private double re;
     private double im;
+
     public ComplexNumber() {
         re = 0.0;
         im = 0.0;
     }
+
+    public ComplexNumber(final double userRe, final double userIm) {
+        re = userRe;
+        im = userIm;
+    }
+
     public double getRe() {
         return re;
     }
--- code/complex-number-calculation/src/test/java/ru/unn/agile/complexnumbercalculation/model/KulyginaJuliaTest.java
+++ code/complex-number-calculation/src/test/java/ru/unn/agile/complexnumbercalculation/model/KulyginaJuliaTest.java
@@ -6,10 +6,58 @@ import static org.junit.Assert.*;
 
 public class KulyginaJuliaTest {
     @Test
-    public void emptyCreateComplexNumber() {
+    public void createComplexNumberByDefaultNotNull() {
+        ComplexNumber a = new ComplexNumber();
+
+        assertNotNull(a);
+    }
+
+    @Test
+    public void emptyCreateComplexNumberCheckRealPart() {
         ComplexNumber a = new ComplexNumber();
 
         assertEquals(0.0, a.getRe(), 0.0);
+    }
+
+    @Test
+    public void emptyCreateComplexNumberCheckImaginePart() {
+        ComplexNumber a = new ComplexNumber();
+
+        assertEquals(0.0, a.getIm(), 0.0);
+    }
+    @Test
+    public void createComplexNumberWithParametersNotNull() {
+        ComplexNumber a = new ComplexNumber(5.0, 6.0);
+
+        assertNotNull(a);
+    }
+
+    @Test
+    public void createComplexNumberWithParametersCheckRe() {
+        ComplexNumber a = new ComplexNumber(5.0, 6.0);
+
+        assertEquals(5.0, a.getRe(), 0.0);
+    }
+
+    @Test
+    public void createComplexNumberWithParametersCheckIm() {
+        ComplexNumber a = new ComplexNumber(5.0, 6.0);
+
+        assertEquals(6.0, a.getIm(), 0.0);
+    }
+
+    @Test
+    public void createComplexNumberWithNegativeParametersCheckRe() {
+        ComplexNumber a = new ComplexNumber(-5.0, 6.0);
+
+        assertEquals(-5.0, a.getRe(), 0.0);
+    }
+
+    @Test
+    public void createComplexNumberWithNegativeParametersCheckIm() {
+        ComplexNumber a = new ComplexNumber(5.0, -6.0);
+
+        assertEquals(-6.0, a.getIm(), 0.0);
     }
 
 }
