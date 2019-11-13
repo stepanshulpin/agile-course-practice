300b09223630e5cdcc52be4802d92bfa237a8332
NorthlaneKek <skater7878@gmail.com>
2019-11-13 22:42:37 +0300

    Красильников -- Лабораторная работа #0

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KrasilnikovMikhailComplexNumberTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KrasilnikovMikhailComplexNumberTest.java
@@ -9,17 +9,12 @@ public class KrasilnikovMikhailComplexNumberTest {
     private double delta = 0.001;
 
     @Test
-    public void testThatComplexNumberNotEqualRealNumber() {
-        int number = 5;
-        ComplexNumber complexNumber = new ComplexNumber(1, 2);
-        assertFalse(complexNumber.equals(number));
-    }
-    @Test
-    public void testThatCanGetRealNumber(){
+    public void testThatCanGetRealNumber() {
         int realNumber = 2;
         ComplexNumber complexNumber = new ComplexNumber(2, 25);
         assertEquals(realNumber, complexNumber.getReal(), delta);
     }
+
     @Test
     public void testThatCanSumComplexNumbers() {
         ComplexNumber resultNumber = new ComplexNumber(10, 50);
@@ -27,6 +22,7 @@ public class KrasilnikovMikhailComplexNumberTest {
         ComplexNumber secondTerm = new ComplexNumber(7, 10);
         assertEquals(resultNumber, firstTerm.add(secondTerm));
     }
+
     @Test
     public void testThatSumIsCommutative() {
         ComplexNumber firstTerm = new ComplexNumber(3, 40);
