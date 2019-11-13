6facb76d9c9e28b7d60dc839ac179755ddcc4251
Julia Kulygina <julia.kulygina@vicuesoft.com>
2019-11-13 18:16:15 +0300

    Refactoring

--- code/complex-number-calculation/src/main/java/ru/unn/agile/complexnumbercalculation/model/CalculatorComplexNumber.java
+++ code/complex-number-calculation/src/main/java/ru/unn/agile/complexnumbercalculation/model/CalculatorComplexNumber.java
@@ -1,42 +1,23 @@
 package ru.unn.agile.complexnumbercalculation.model;
 
 public class CalculatorComplexNumber {
-    private ComplexNumber z1;
-    private ComplexNumber z2;
-
     public CalculatorComplexNumber() {
-        z1 = new ComplexNumber();
-        z2 = new ComplexNumber();
     }
 
-
-    public CalculatorComplexNumber(final ComplexNumber userZ1, final ComplexNumber userZ2) {
-       z1 = userZ1;
-       z2 = userZ2;
-    }
-
-    public ComplexNumber getZ1() {
-        return z1;
-    }
-
-    public ComplexNumber getZ2() {
-        return z2;
-    }
-
-    public ComplexNumber addition() {
+    public ComplexNumber addition(final ComplexNumber z1, final ComplexNumber z2) {
         return new ComplexNumber(z1.getRe() + z2.getRe(), z1.getIm() + z2.getIm());
     }
 
-    public ComplexNumber subtraction() {
+    public ComplexNumber subtraction(final ComplexNumber z1, final ComplexNumber z2) {
         return new ComplexNumber(z1.getRe() - z2.getRe(), z1.getIm() - z2.getIm());
     }
 
-    public ComplexNumber multiplication() {
+    public ComplexNumber multiplication(final ComplexNumber z1, final ComplexNumber z2) {
         return new ComplexNumber(z2.getRe() * z1.getRe() - z2.getIm() * z1.getIm(),
                 z2.getRe() * z1.getIm() + z2.getIm() * z1.getRe());
     }
 
-    public ComplexNumber division() {
+    public ComplexNumber division(final ComplexNumber z1, final ComplexNumber z2) {
         if (z2.equals(new ComplexNumber())) {
             throw new ArithmeticException("Can`t to be divided on zero");
         }
--- code/complex-number-calculation/src/test/java/ru/unn/agile/complexnumbercalculation/model/CalculatorComplexNumberTest.java
+++ code/complex-number-calculation/src/test/java/ru/unn/agile/complexnumbercalculation/model/CalculatorComplexNumberTest.java
@@ -13,56 +13,13 @@ public class CalculatorComplexNumberTest {
     }
 
     @Test
-    public void createCalculatorComplexNumberByDefaultCheckZeroNumbers1() {
-        CalculatorComplexNumber a = new CalculatorComplexNumber();
-        ComplexNumber z1 = new ComplexNumber();
-
-        assertEquals(z1, a.getZ1());
-    }
-
-    @Test
-    public void createCalculatorComplexNumberByDefaultCheckZeroNumbers2() {
-        CalculatorComplexNumber a = new CalculatorComplexNumber();
-        ComplexNumber z1 = new ComplexNumber();
-
-        assertEquals(z1, a.getZ2());
-    }
-
-    @Test
-    public void createCalculatorComplexNumberByComplexNumbersNotNull() {
-        ComplexNumber z1 = new ComplexNumber(5.0, 6.0);
-        ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
-
-        assertNotNull(a);
-    }
-
-    @Test
-    public void createCalculatorComplexNumberByComplexNumbersCheck1() {
-        ComplexNumber z1 = new ComplexNumber(5.0, 6.0);
-        ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
-
-        assertEquals(z1, a.getZ1());
-    }
-
-    @Test
-    public void createCalculatorComplexNumberByComplexNumbersCheck2() {
-        ComplexNumber z1 = new ComplexNumber(5.0, 6.0);
-        ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
-
-        assertEquals(z2, a.getZ2());
-    }
-
-    @Test
     public void operationAdditionalCalculatorComplexNumber() {
         ComplexNumber z1 = new ComplexNumber(5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(9.0, 12.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.addition();
+        ComplexNumber result = a.addition(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -72,9 +29,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(-1.0, 12.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.addition();
+        ComplexNumber result = a.addition(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -84,9 +41,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber();
         ComplexNumber expectedResult = new ComplexNumber(-5.0, 6.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.addition();
+        ComplexNumber result = a.addition(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -96,9 +53,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(1.0, -1.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.subtraction();
+        ComplexNumber result = a.subtraction(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -108,9 +65,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(-9.0, 0.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.subtraction();
+        ComplexNumber result = a.subtraction(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -120,9 +77,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber();
         ComplexNumber expectedResult = new ComplexNumber(-5.0, 6.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.subtraction();
+        ComplexNumber result = a.subtraction(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -132,9 +89,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(-10.0, 50.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.multiplication();
+        ComplexNumber result = a.multiplication(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -144,9 +101,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(-50.0, -10.0);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.multiplication();
+        ComplexNumber result = a.multiplication(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -156,9 +113,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(-5.0, 6.0);
         ComplexNumber z2 = new ComplexNumber();
         ComplexNumber expectedResult = new ComplexNumber();
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.multiplication();
+        ComplexNumber result = a.multiplication(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -168,9 +125,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber(4.0, 6.0);
         ComplexNumber expectedResult = new ComplexNumber(0.9615384615384616, -0.19230769230769232);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.division();
+        ComplexNumber result = a.division(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -180,9 +137,9 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber(4.0, -6.0);
         ComplexNumber expectedResult = new ComplexNumber(-0.19230769230769232, 0.9615384615384616);
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.division();
+        ComplexNumber result = a.division(z1, z2);
 
         assertEquals(expectedResult, result);
     }
@@ -192,8 +149,8 @@ public class CalculatorComplexNumberTest {
         ComplexNumber z1 = new ComplexNumber(5.0, 5.0);
         ComplexNumber z2 = new ComplexNumber();
 
-        CalculatorComplexNumber a = new CalculatorComplexNumber(z1, z2);
+        CalculatorComplexNumber a = new CalculatorComplexNumber();
 
-        ComplexNumber result = a.division();
+        ComplexNumber result = a.division(z1, z2);
     }
 }
