92a9cfa9a4bb0c58a5e05921ad10bc9b218b3ac4
Vladimir Tyagunov <tyagunov19@gmail.com>
2019-11-15 00:52:31 +0300

    refactor polygon class
    add size and area fields, add method calculateArea

--- code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
+++ code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
@@ -4,28 +4,36 @@ import java.util.List;
 
 public class Polygon {
     private final Point[] vertices;
+    private final int size;
+    private final double area;
 
     public Polygon(Point... points) {
         if (points.length < 3)
             throw new IllegalArgumentException("A polygon must have at least three vertices");
 
         this.vertices = points;
+        this.size = points.length;
+        this.area = this.calculateArea();
     }
 
     public int getSize() {
-        return vertices.length;
+        return size;
     }
 
     public double getArea() {
+        return this.area;
+    }
+
+    private double calculateArea() {
         double area = 0;
 
-        for (int i = 0; i < vertices.length - 1; i++) {
+        for (int i = 0; i < this.size - 1; i++) {
             area += Math.abs((vertices[i].getX() + vertices[i+1].getX()) *
                     (vertices[i].getY() - vertices[i+1].getY()));
         }
 
-        area += Math.abs((vertices[vertices.length - 1].getX() + vertices[0].getX()) *
-                (vertices[vertices.length - 1].getY() - vertices[0].getY()));
+        area += Math.abs((vertices[this.size - 1].getX() + vertices[0].getX()) *
+                (vertices[this.size - 1].getY() - vertices[0].getY()));
 
         area = area / 2;
 
