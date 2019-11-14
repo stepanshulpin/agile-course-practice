62daf0ff1cd8bc46f49681a1fb24afc95de76917
Vladimir Tyagunov <tyagunov19@gmail.com>
2019-11-14 23:46:34 +0300

    add Polygon constructor with any number of points

--- code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
+++ code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
@@ -3,12 +3,9 @@ package ru.unn.agile.polygon.model;
 import java.util.List;
 
 public class Polygon {
-    private final Point[] points = new Point[3];
+    private final Point[] points;
 
-    public Polygon(Point pointA, Point pointB, Point pointC) {
-        this.points[0] = pointA;
-        this.points[1] = pointB;
-        this.points[2] = pointC;
-
+    public Polygon(Point... points) {
+        this.points = points;
     }
 }
--- code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PolygonTest.java
+++ code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PolygonTest.java
@@ -18,4 +18,19 @@ public class PolygonTest {
             fail("Should not have thrown any exception");
         }
     }
+
+    @Test
+    public void canCreateSquare() {
+        Point pointA = new Point();
+        Point pointB = new Point(0, 2);
+        Point pointC = new Point(2, 2);
+        Point pointD = new Point(2, 0);
+
+        try{
+            Polygon triangle = new Polygon(pointA, pointB, pointC, pointD);
+        }
+        catch(Exception e){
+            fail("Should not have thrown any exception");
+        }
+    }
 }
