5e52e7c6250cbd88c9942ac64716769e35e14f8e
Vladimir Tyagunov <tyagunov19@gmail.com>
2019-11-15 00:06:04 +0300

    fix Polygon constructor

--- code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
+++ code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Polygon.java
@@ -3,9 +3,10 @@ package ru.unn.agile.polygon.model;
 import java.util.List;
 
 public class Polygon {
-    private final Point[] points;
+    private final Point[] vertices;
 
-    public Polygon(Point... points) {
-        this.points = points;
+    public Polygon(Point... points) throws Exception {
+        if (points.length < 3) throw new Exception("A polygon must have at least three vertices");
+        this.vertices = points;
     }
 }
--- code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PolygonTest.java
+++ code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PolygonTest.java
@@ -2,6 +2,7 @@ package ru.unn.agile.polygon.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class PolygonTest {
@@ -27,10 +28,20 @@ public class PolygonTest {
         Point pointD = new Point(2, 0);
 
         try{
-            Polygon triangle = new Polygon(pointA, pointB, pointC, pointD);
+            Polygon square = new Polygon(pointA, pointB, pointC, pointD);
         }
         catch(Exception e){
             fail("Should not have thrown any exception");
         }
+    }
+
+    @Test(expected = Exception.class)
+    public void cantCreatePolygonWithLessThanThreePoints() throws Exception {
+        Point pointA = new Point();
+        Point pointB = new Point(0, 2);
+
+        Polygon pol1 = new Polygon();
+        Polygon pol2 = new Polygon(pointA);
+        Polygon pol3 = new Polygon(pointA, pointB);
     }
 }
