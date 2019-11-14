c0a866de547dbdc6dba8eb1367ac357f9fa1911b
Alexander Nesterov <nesoldr@mail.ru>
2019-11-14 19:34:43 -0100

    Added test canSetBitInBitArray

--- code/bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
+++ code/bit-array/src/main/java/ru/unn/agile/bitarray/model/BitArray.java
@@ -7,18 +7,25 @@ public class BitArray {
     public BitArray(int count_bit) {
         int length_raw_array = -1;
         if (count_bit > 0)
-        {
-            length_raw_array = count_bit / 8;
-            if (count_bit % 8 != 0) length_raw_array++;
-        }
-        this.raw_array = new byte[length_raw_array];
+            length_raw_array = getNumElem(count_bit);
+        raw_array = new byte[length_raw_array];
 
-        for (int i=0; i<this.raw_array.length; i++)  {
-            this.raw_array[i] = 0;
+        for (int i=0; i<raw_array.length; i++)  {
+            raw_array[i] = 0;
         }
     }
 
     public byte[] getRawArray() {
-        return this.raw_array;
+        return raw_array;
+    }
+    
+    private int getNumElem(int bit) {
+        int num_raw_array = bit / 8;
+        if (bit % 8 != 0) num_raw_array++;
+        return num_raw_array;
+    }
+
+    public void setBit(int bit) {
+        raw_array[getNumElem(bit) - 1] |= 1 << (bit % 8);
     }
 }
--- code/bit-array/src/test/java/ru/unn/agile/bitarray/test/BitArrayTest.java
+++ code/bit-array/src/test/java/ru/unn/agile/bitarray/test/BitArrayTest.java
@@ -36,4 +36,25 @@ public class BitArrayTest {
         for (int elem : raw_array)
             assertEquals(elem, 0);
     }
+
+    @Test
+    public void canSetBitInBitArray() {
+        // arrange
+        BitArray btr = new BitArray(41);
+
+        // act
+        btr.setBit(4);
+        btr.setBit(13);
+        btr.setBit(19);
+        btr.setBit(25);
+        btr.setBit(34);
+        btr.setBit(41);
+        byte[]actual = btr.getRawArray();
+
+        // assert
+        byte[] reference = {16, 32, 8, 2, 4, 2};
+        for(int i = 0; i < actual.length; i++)
+            assertEquals(reference[i], actual[i]);
+    }
+
 }
