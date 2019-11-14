8e3a243349a250b53985f3d93f5b095f694d6fd1
Vladimir Tyagunov <tyagunov19@gmail.com>
2019-11-14 23:09:06 +0300

    add getters for Point class

--- code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Point.java
+++ code/polygon-area/src/main/java/ru/unn/agile/polygon/model/Point.java
@@ -1,4 +1,24 @@
 package ru.unn.agile.polygon.model;
 
 public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
--- code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PointTest.java
+++ code/polygon-area/src/test/java/ru/unn/agile/polygon/model/PointTest.java
@@ -5,9 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PointTest {
+
+    private final double delta = 0.001;
+
     @Test
-    public void canCreatePointWithoutInitialValues() {
-        Point point = new Point();
-        assertEquals(new Point(0, 0), point);
+    public void canGetXValueFromPointZero() {
+        Point pointA = new Point();
+        Point pointB = new Point(0, 0);
+        assertEquals(0, pointA.getX(), delta);
+        assertEquals(0, pointB.getX(), delta);
     }
+
+    @Test
+    public void canGetYValueFromPointZero() {
+        Point pointA = new Point();
+        Point pointB = new Point(0, 0);
+        assertEquals(0, pointA.getY(), delta);
+        assertEquals(0, pointB.getY(), delta);
+    }
+}
 }
