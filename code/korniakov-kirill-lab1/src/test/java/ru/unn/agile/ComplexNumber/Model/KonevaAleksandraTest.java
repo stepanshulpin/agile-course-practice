84d3ceb09759cd1bfa808d4cd4496189df4d6231
Edvard-Hagerup-Grieg <mari.louis.von.franz@gmail.com>
2019-11-16 15:24:08 +0300

    fix AAA style

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KonevaAleksandraTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KonevaAleksandraTest.java
@@ -9,7 +9,9 @@ public class KonevaAleksandraTest {
     public void canAddIdentityElement() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(0, 0);
+
         ComplexNumber sum = z1.add(z2);
+
         assertEquals(z1, sum);
     }
 
@@ -17,7 +19,9 @@ public class KonevaAleksandraTest {
     public void canAddInverseElement() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(-1, -2);
+
         ComplexNumber sum = z1.add(z2);
+
         assertEquals(new ComplexNumber(0, 0), sum);
     }
 
@@ -25,8 +29,10 @@ public class KonevaAleksandraTest {
     public void isAddCommutative() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(3, 4);
+
         ComplexNumber sum1 = z1.add(z2);
         ComplexNumber sum2 = z2.add(z1);
+
         assertEquals(sum1, sum2);
     }
 
@@ -34,7 +40,9 @@ public class KonevaAleksandraTest {
     public void canMultiplyIdentityElement() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(1, 0);
+
         ComplexNumber sum = z1.multiply(z2);
+
         assertEquals(z1, sum);
     }
 
@@ -50,8 +58,10 @@ public class KonevaAleksandraTest {
     public void isMultiplyCommutative() {
         ComplexNumber z1 = new ComplexNumber(1, 2);
         ComplexNumber z2 = new ComplexNumber(3, 4);
+
         ComplexNumber mult1 = z1.multiply(z2);
         ComplexNumber mult2 = z2.multiply(z1);
+
         assertEquals(mult1, mult2);
     }
 }
