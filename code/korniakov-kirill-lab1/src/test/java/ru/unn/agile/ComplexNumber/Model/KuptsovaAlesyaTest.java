920b6be171d60106a3f2114cfe732f900874bb92
Alesya Kuptsova <a5484@icloud.com>
2019-11-17 01:58:20 +0300

    Bild fixed.

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KuptsovaAlesyaTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/KuptsovaAlesyaTest.java
@@ -9,40 +9,39 @@ import static java.lang.String.valueOf;
 import static junit.framework.TestCase.assertEquals;
 
 public class KuptsovaAlesyaTest {
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
-    @Test
-    public void canNotCreateComplexNumberWithUncorrectValues() {
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotCreateComplexNumberWithIncorrectValues() {
         String re = "3.14";
         String im = "-rjfho;i";
-        thrown.expect(IllegalArgumentException.class);
         ComplexNumber z = new ComplexNumber(re, im);
-    }
-    @Test
-    public void canGetRealFromComplexNumberWithARealPart() {
-        String re = "3.14";
-        String im = "-13.0";
-        ComplexNumber z = new ComplexNumber(re, im);
-        double num = z.getReal();
-        String numstr = valueOf(num);
-        assertEquals(re, numstr);
-    }
-    @Test
-    public void canGetImaginaryFromComplexNumberWithImaginaryPart() {
-        String re = "3.14";
-        String im = "-13.0";
-        ComplexNumber z = new ComplexNumber(re, im);
-        double num = z.getImaginary();
-        String numstr = valueOf(num);
-        assertEquals(im, numstr);
     }
 
     @Test
-    public void canMultiplyNumbersWithNegativePartS() {
+    public void canGetRealFromComplexNumberWithRealPart() {
+        double re = .14;
+        double im = -13.0;
+        ComplexNumber z = new ComplexNumber(re, im);
+        double num = z.getReal();
+        assertEquals(re, num);
+    }
+
+    @Test
+    public void canGetImaginaryFromComplexNumberWithImaginaryPart() {
+        double re = 3.14;
+        double im = -13.0;
+        ComplexNumber z = new ComplexNumber(re, im);
+        double num = z.getImaginary();
+        assertEquals(im, num);
+    }
+
+    @Test
+    public void canMultiplyNumbersWithNegativePart() {
         ComplexNumber z1 = new ComplexNumber(-1, 4);
         ComplexNumber z2 = new ComplexNumber(-2, -3);
-        ComplexNumber num = z1.multiply(z2);
-        assertEquals(new ComplexNumber(14, -5), num);
+        ComplexNumber actualValue = z1.multiply(z2);
+        ComplexNumber expectedValue = new ComplexNumber(14, -5);
+        assertEquals(expectedValue, actualValue);
     }
 }
+
