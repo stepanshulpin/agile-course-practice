909a51c1d486c813edfe0c428cc4b54c83ae9036
Kalyanov Dmitry <kalyanowdima@mail.ru>
2019-11-17 11:57:41 +0300

    Init commit. Files have been created

---
+++ code/string-calculator/src/main/java/ru/unn/agile/calc/model/StringCalc.java
@@ -0,0 +1,5 @@
+package ru.unn.agile.calc.model;
+
+public class StringCalc {
+
+}
---
+++ code/string-calculator/src/test/java/ru/unn/agile/calc/model/StringCalcTest.java
@@ -0,0 +1,13 @@
+package ru.unn.agile.calc.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringCalcTest {
+
+    @Test
+    public void canBeBuilt() {
+        assertTrue(true);
+    }
+}
