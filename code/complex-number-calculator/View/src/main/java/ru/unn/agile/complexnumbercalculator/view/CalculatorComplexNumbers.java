d82bbed7cfd8ae1ebdb004323671708b7fb23387
Anna-Mironova <anyt.ka@bk.ru>
2019-11-23 15:15:42 +0300

    Form created

--- code/complex-number-calculator/View/src/main/java/ru/unn/agile/complexnumbercalculator/view/CalculatorComplexNumbers.java
+++ code/complex-number-calculator/View/src/main/java/ru/unn/agile/complexnumbercalculator/view/CalculatorComplexNumbers.java
@@ -1,4 +1,41 @@
 package ru.unn.agile.complexnumbercalculator.view;
 
+import javax.swing.*;
+
 public class CalculatorComplexNumbers {
+    private JPanel mainPanel;
+    private JPanel chooseOperationPanel;
+    private JPanel enterDataPanel;
+    private JPanel calculationPanel;
+    private JLabel chooseLabel;
+    private JComboBox operationsСomboBox;
+    private JLabel enterDataLabel;
+    private JLabel resultLabel;
+    private JButton calculateButton;
+    private JTextField resultTextField;
+    private JTextField firstReTextField;
+    private JTextField firstImTextField;
+    private JTextField secondReTextField;
+    private JTextField secondImTextField;
+    private JTextField degeeTextField;
+    private JLabel fisrtReLabel;
+    private JLabel fisrtImLabel;
+    private JLabel secondReLabel;
+    private JLabel secondImLabel;
+    private JLabel degreeLabel;
+    private JPanel errorPanel;
+    private JLabel errorLabel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CalculatorComplexNumbers");
+        frame.setContentPane(new CalculatorComplexNumbers().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
