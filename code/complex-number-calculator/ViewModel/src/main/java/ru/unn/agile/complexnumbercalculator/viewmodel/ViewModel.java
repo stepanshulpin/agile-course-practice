8fa0e7a8238db936e72bbfb795feb2a1467834ae
Anna-Mironova <anyt.ka@bk.ru>
2019-11-23 16:55:30 +0300

    Added ViewModel directory and test for default values

---
+++ code/complex-number-calculator/ViewModel/src/main/java/ru/unn/agile/complexnumbercalculator/viewmodel/ViewModel.java
@@ -0,0 +1,74 @@
+package ru.unn.agile.complexnumbercalculator.viewmodel;
+
+public class ViewModel {
+    private Operations operations;
+    private String result;
+    private String firstRe;
+    private String firstIm;
+    private String secondRe;
+    private String secondIm;
+    private String degree;
+    private String errorText;
+
+    public enum Operations {
+        ADD ("Сложить"),
+        SUBTRACT ("Вычесть"),
+        MULTIPLY ("Умножить"),
+        DIVIDE ("Разделить"),
+        POW ("Возвести в степень"),
+        ROOT ("Извлечь корень"),
+        CONJUGATION("Найти сопряженное");
+        private final String name;
+
+        Operations(final String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return name;
+        }
+    }
+
+    public ViewModel(){
+        result = "";
+        firstRe = "";
+        firstIm = "";
+        secondRe = "";
+        secondIm = "";
+        degree = "";
+        operations = operations.ADD;
+        errorText = "";
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+    public String getFirstRe(){
+        return firstRe;
+    }
+
+    public String getFirstIm(){
+        return firstIm;
+    }
+
+    public String getSecondRe(){
+        return secondRe;
+    }
+
+    public String getSecondIm(){
+        return secondIm;
+    }
+
+    public String getDegree(){
+        return degree;
+    }
+
+    public Operations getOperations() {
+        return operations;
+    }
+
+    public String getError(){
+        return errorText;
+    }
+}
---
+++ code/complex-number-calculator/ViewModel/src/test/java/ru/unn/agile/complexnumbercalculator/viewmodel/ViewModelTests.java
@@ -0,0 +1,34 @@
+package ru.unn.agile.complexnumbercalculator.viewmodel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import ru.unn.agile.complexnumbercalculator.viewmodel.ViewModel.Operations;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultValues() {
+        assertEquals(Operations.ADD, viewModel.getOperations());
+        assertEquals("", viewModel.getFirstRe());
+        assertEquals("", viewModel.getFirstIm());
+        assertEquals("", viewModel.getSecondRe());
+        assertEquals("", viewModel.getSecondIm());
+        assertEquals("", viewModel.getDegree());
+        assertEquals("", viewModel.getError());
+        assertEquals("", viewModel.getResult());
+    }
+}
