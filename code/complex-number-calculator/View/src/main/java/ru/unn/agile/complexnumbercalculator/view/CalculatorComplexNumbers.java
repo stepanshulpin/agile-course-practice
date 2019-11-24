3868ecbc09261c23141b40248cc54b38fb7cd572
Anna-Mironova <anyt.ka@bk.ru>
2019-11-24 18:38:49 +0300

    Fix style

--- code/complex-number-calculator/View/src/main/java/ru/unn/agile/complexnumbercalculator/view/CalculatorComplexNumbers.java
+++ code/complex-number-calculator/View/src/main/java/ru/unn/agile/complexnumbercalculator/view/CalculatorComplexNumbers.java
@@ -8,13 +8,13 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class CalculatorComplexNumbers {
+public final class CalculatorComplexNumbers {
     private JPanel mainPanel;
     private JPanel chooseOperationPanel;
     private JPanel enterDataPanel;
     private JPanel calculationPanel;
     private JLabel chooseLabel;
-    private JComboBox operationsComboBox;
+    private JComboBox<ViewModel.Operations> operationsComboBox;
     private JLabel enterDataLabel;
     private JButton calculateButton;
     private JTextField firstReTextField;
@@ -44,14 +44,14 @@ public class CalculatorComplexNumbers {
         frame.setResizable(false);
     }
 
-    private CalculatorComplexNumbers(final ViewModel viewModel){
+    private CalculatorComplexNumbers(final ViewModel viewModel) {
         this.viewModel = viewModel;
         backBind();
         loadOperations();
         hideUnnecessaryElements();
         calculateButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(final ActionEvent e) {
                 bind();
                 CalculatorComplexNumbers.this.viewModel.calculate();
                 backBind();
@@ -60,7 +60,7 @@ public class CalculatorComplexNumbers {
 
         operationsComboBox.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(final ActionEvent e) {
                 bind();
                 CalculatorComplexNumbers.this.viewModel.hideUnnecessaryFields();
                 hideUnnecessaryElements();
@@ -83,29 +83,25 @@ public class CalculatorComplexNumbers {
         degreeTextField.addKeyListener(keyListener);
     }
 
-    private void loadOperations(){
+    private void loadOperations() {
         ViewModel.Operations[] operations = ViewModel.Operations.values();
         operationsComboBox.setModel(new JComboBox<>(operations).getModel());
     }
 
-    private void hideUnnecessaryElements(){
-        if (!viewModel.isDegreeVisible())
-        {
+    private void hideUnnecessaryElements() {
+        if (!viewModel.isDegreeVisible()) {
             degreeTextField.setVisible(false);
             degreeLabel.setVisible(false);
-        }
-        else{
+        } else {
             degreeTextField.setVisible(true);
             degreeLabel.setVisible(true);
         }
-        if (!viewModel.isSecondNumberVisible())
-        {
+        if (!viewModel.isSecondNumberVisible()) {
             secondReTextField.setVisible(false);
             secondReLabel.setVisible(false);
             secondImTextField.setVisible(false);
             secondImLabel.setVisible(false);
-        }
-        else{
+        } else {
             secondReTextField.setVisible(true);
             secondReLabel.setVisible(true);
             secondImTextField.setVisible(true);
@@ -113,18 +109,18 @@ public class CalculatorComplexNumbers {
         }
     }
 
-    private void backBind(){
+    private void backBind() {
         calculateButton.setEnabled(viewModel.isCalculateButtonEnabled());
         resultLabel.setText(viewModel.getResult());
         errorLabel.setText(viewModel.getError());
     }
 
-    private void bind(){
+    private void bind() {
         viewModel.setFirstRe(firstReTextField.getText());
         viewModel.setFirstIm(firstImTextField.getText());
         viewModel.setSecondRe(secondReTextField.getText());
         viewModel.setSecondIm(secondImTextField.getText());
         viewModel.setDegree(degreeTextField.getText());
-        viewModel.setOperations((ViewModel.Operations)operationsComboBox.getSelectedItem());
+        viewModel.setOperations((ViewModel.Operations) operationsComboBox.getSelectedItem());
     }
 }
