a0e25de37f8dbc6ca3e9b277aa8c6370a02c4473
Vaginak Simonyan <vaginak.simonyan.consultant@nielsen.com>
2019-11-30 18:47:50 +0300

    Added ViewModel realization with tests

--- code/datastructure-set/ViewModel/src/main/java/ru/unn/agile/datastructure/set/viewmodel/ViewModel.java
+++ code/datastructure-set/ViewModel/src/main/java/ru/unn/agile/datastructure/set/viewmodel/ViewModel.java
@@ -1,5 +1,151 @@
 package ru.unn.agile.datastructure.set.viewmodel;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import ru.unn.agile.datastructure.set.model.MySet;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class ViewModel {
 
+    private BooleanProperty btnAddEnabledProp = new SimpleBooleanProperty();
+    private BooleanProperty btnRemoveEnabledProp = new SimpleBooleanProperty();
+    private BooleanProperty btnRetainEnabledProp = new SimpleBooleanProperty();
+    private BooleanProperty btnContainsEnabledProp = new SimpleBooleanProperty();
+    private StringProperty txtContainsItemProp = new SimpleStringProperty();
+    private StringProperty txtIsEmptySetProp = new SimpleStringProperty();
+    private StringProperty txtEnteredItemsProp = new SimpleStringProperty();
+    private StringProperty txtCurrentSetProp = new SimpleStringProperty();
+    private MySet<Integer> currentSet = new MySet<>();
+    private Set<Integer> enteredSet = new LinkedHashSet<>();
+
+    public ViewModel() {
+        btnAddEnabledProp.setValue(false);
+        btnRemoveEnabledProp.setValue(false);
+        btnRetainEnabledProp.setValue(false);
+        btnContainsEnabledProp.setValue(false);
+
+        txtContainsItemProp.setValue("");
+        txtIsEmptySetProp.setValue("");
+        txtEnteredItemsProp.setValue("");
+        txtCurrentSetProp.setValue("");
+
+        txtEnteredItemsProp.addListener((observable, oldValue, newValue) -> {
+            setEnteredItems(newValue);
+        });
+    }
+
+    public boolean isAddButtonEnable() {
+        return btnAddEnabledProp.get();
+    }
+
+    public boolean isRemoveButtonEnable() {
+        return btnRemoveEnabledProp.get();
+    }
+
+    public boolean isContainsButtonEnable() {
+        return btnContainsEnabledProp.get();
+    }
+
+    public boolean isRetainButtonEnable() {
+        return btnRetainEnabledProp.get();
+    }
+
+    public StringProperty getTxtEnteredItemsProp() {
+        return txtEnteredItemsProp;
+    }
+
+    public StringProperty getTxtCurrentSetProp() {
+        return txtCurrentSetProp;
+    }
+
+    public StringProperty getTxtContainsItemProp() {
+        return txtContainsItemProp;
+    }
+
+    public StringProperty getTxtIsEmptyItemProp() {
+        return txtIsEmptySetProp;
+    }
+
+    private void setEnteredItems(String itemsStr) {
+        enteredSet.clear();
+        txtEnteredItemsProp.setValue(itemsStr);
+
+        var isValidEnteredData = false;
+
+        if (!itemsStr.isEmpty()) {
+            var splitedItems = itemsStr.split(" ");
+            if (splitedItems.length > 0) {
+                for (String splitedItem : splitedItems) {
+                    try {
+                        int parsedItem = Integer.parseInt(splitedItem);
+                        enteredSet.add(parsedItem);
+                    } catch (NumberFormatException e) {
+                        break;
+                    }
+                }
+                isValidEnteredData = splitedItems.length == enteredSet.size();
+            }
+        }
+
+        btnAddEnabledProp.setValue(isValidEnteredData);
+        btnRemoveEnabledProp.setValue(isValidEnteredData);
+        btnRetainEnabledProp.setValue(isValidEnteredData);
+        btnContainsEnabledProp.setValue(isValidEnteredData);
+    }
+
+    private String getCurrentSetItems() {
+        if (currentSet.isEmpty()) {
+            return "";
+        }
+        var builder = new StringBuilder();
+        var iterator = currentSet.iterator();
+        while (iterator.hasNext()) {
+            builder.append(iterator.next());
+            builder.append(" ");
+        }
+        builder.deleteCharAt(builder.length() - 1);
+        return builder.toString();
+    }
+
+    public void addEnteredItems() {
+        currentSet.addAll(enteredSet);
+        txtCurrentSetProp.setValue(getCurrentSetItems());
+    }
+
+    public void removeEnteredItems() {
+        currentSet.removeAll(enteredSet);
+        txtCurrentSetProp.setValue(getCurrentSetItems());
+    }
+
+    public void retainEnteredItems() {
+        currentSet.retainAll(enteredSet);
+        txtCurrentSetProp.setValue(getCurrentSetItems());
+    }
+
+    public void clearCurrentItems() {
+        currentSet.clear();
+        txtCurrentSetProp.setValue(getCurrentSetItems());
+    }
+
+    public void containsEnteredItems() {
+        if (currentSet.containsAll(enteredSet)) {
+            txtContainsItemProp.setValue("Yes");
+        }
+        else {
+            txtContainsItemProp.setValue("No");
+        }
+    }
+
+    public void isCurrentSetEmpty() {
+        if (currentSet.isEmpty()) {
+            txtIsEmptySetProp.setValue("Yes");
+        }
+        else {
+            txtIsEmptySetProp.setValue("No");
+        }
+    }
 }
--- code/datastructure-set/ViewModel/src/test/java/ru/unn/agile/datastructure/set/viewmodel/ViewModelTests.java
+++ code/datastructure-set/ViewModel/src/test/java/ru/unn/agile/datastructure/set/viewmodel/ViewModelTests.java
@@ -1,5 +1,130 @@
 package ru.unn.agile.datastructure.set.viewmodel;
 
-public class ViewModelTests {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+public class ViewModelTests {
+    private ViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void tearDown() {
+        viewModel = null;
+    }
+
+    @Test
+    public void ifNoEnteredItemsAddButtonIsDisabled() {
+        assertFalse(viewModel.isAddButtonEnable());
+    }
+
+    @Test
+    public void ifNoEnteredItemsRemoveButtonIsDisabled() {
+        assertFalse(viewModel.isRemoveButtonEnable());
+    }
+
+    @Test
+    public void ifNoEnteredItemsContainsButtonIsDisabled() {
+        assertFalse(viewModel.isContainsButtonEnable());
+    }
+
+    @Test
+    public void ifNoEnteredItemsRetainButtonIsDisabled() {
+        assertFalse(viewModel.isRetainButtonEnable());
+    }
+
+    @Test
+    public void ifEnteredItemIsValidAddButtonIsEnabled() {
+        viewModel.getTxtEnteredItemsProp().setValue("10");
+        assertTrue(viewModel.isAddButtonEnable());
+    }
+
+    @Test
+    public void ifEnteredItemIsNotValidAddButtonIsDisabled() {
+        viewModel.getTxtEnteredItemsProp().setValue("ds");
+        assertFalse(viewModel.isAddButtonEnable());
+    }
+
+    @Test
+    public void ifEnteredItemsAreValidAddButtonIsEnabled() {
+        viewModel.getTxtEnteredItemsProp().setValue("10 52 -3 95");
+        assertTrue(viewModel.isAddButtonEnable());
+    }
+
+    @Test
+    public void ifEnteredItemsAreNotValidAddButtonIsDisabled() {
+        viewModel.getTxtEnteredItemsProp().setValue("50 d 98 3s");
+        assertFalse(viewModel.isAddButtonEnable());
+    }
+
+    @Test
+    public void canAddItemsToSet() {
+        viewModel.getTxtEnteredItemsProp().setValue("12 6 92 4");
+        viewModel.addEnteredItems();
+        assertEquals("12 6 92 4", viewModel.getTxtCurrentSetProp().getValue());
+    }
+
+    @Test
+    public void canRemoveItemsFromSet() {
+        viewModel.getTxtEnteredItemsProp().setValue("2 16 9 4");
+        viewModel.addEnteredItems();
+        viewModel.getTxtEnteredItemsProp().setValue("2 9");
+        viewModel.removeEnteredItems();
+        assertEquals("16 4", viewModel.getTxtCurrentSetProp().getValue());
+    }
+
+    @Test
+    public void canRetainItemsFormSet() {
+        viewModel.getTxtEnteredItemsProp().setValue("5 96 35 21");
+        viewModel.addEnteredItems();
+        viewModel.getTxtEnteredItemsProp().setValue("21 96 3");
+        viewModel.retainEnteredItems();
+        assertEquals("96 21", viewModel.getTxtCurrentSetProp().getValue());
+    }
+
+    @Test
+    public void canClearCurrentSet() {
+        viewModel.getTxtEnteredItemsProp().setValue("5 96 35 21");
+        viewModel.addEnteredItems();
+        viewModel.clearCurrentItems();
+        assertEquals("", viewModel.getTxtCurrentSetProp().getValue());
+    }
+
+    @Test
+    public void canCheckItemsInSetIfContain() {
+        viewModel.getTxtEnteredItemsProp().setValue("5 96 35 21");
+        viewModel.addEnteredItems();
+        viewModel.getTxtEnteredItemsProp().setValue("21 96 5");
+        viewModel.containsEnteredItems();
+        assertEquals("Yes", viewModel.getTxtContainsItemProp().getValue());
+    }
+
+    @Test
+    public void canCheckItemsInSetIfNotContain() {
+        viewModel.getTxtEnteredItemsProp().setValue("5 9 3 1");
+        viewModel.addEnteredItems();
+        viewModel.getTxtEnteredItemsProp().setValue("5 4 3");
+        viewModel.containsEnteredItems();
+        assertEquals("No", viewModel.getTxtContainsItemProp().getValue());
+    }
+
+    @Test
+    public void canCheckSetIsEmptyIfEmpty() {
+        viewModel.isCurrentSetEmpty();
+        assertEquals("Yes", viewModel.getTxtIsEmptyItemProp().getValue());
+    }
+
+    @Test
+    public void canCheckSetIsEmptyIfNotEmpty() {
+        viewModel.getTxtEnteredItemsProp().setValue("5 4");
+        viewModel.addEnteredItems();
+        viewModel.isCurrentSetEmpty();
+        assertEquals("No", viewModel.getTxtIsEmptyItemProp().getValue());
+    }
 }
