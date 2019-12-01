4911e89b34437a291eace5ebf5ec526aef12f56f
Mitrokhin <MitrokhinNP@gmail.ru>
2019-12-01 13:16:49 +0300

    Added form for View

--- code/binary-search/View/src/main/java/ru/unn/agile/binarysearch/view/Search.java
+++ code/binary-search/View/src/main/java/ru/unn/agile/binarysearch/view/Search.java
@@ -1,4 +1,20 @@
 package ru.unn.agile.binarysearch.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Search {
+
+    @FXML
+    private TextField arrayInput;
+    @FXML
+    private TextField elementInput;
+    @FXML
+    private Button buttonSearch;
+    @FXML
+    private Label labelStatus;
+    @FXML
+    private Label labelResult;
 }
