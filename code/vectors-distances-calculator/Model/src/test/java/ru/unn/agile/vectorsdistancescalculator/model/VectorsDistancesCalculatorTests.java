06720c522c32addfe64bea76c36fe07e314749ed
Max <thanatormax@gmail.com>
2019-12-05 01:00:05 +0300

    Project is configured, View FXML is filled

---
+++ code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
@@ -0,0 +1,5 @@
+package ru.unn.agile.vectorsdistancescalculator.model;
+
+public class VectorsDistancesCalculatorTests {
+
+}
---
+++ code/vectors-distances-calculator/View/src/main/java/ru/unn/agile/vectorsdistancescalculator/view/VectorsDistancesCalculator.java
@@ -0,0 +1,4 @@
+package ru.unn.agile.vectorsdistancescalculator.view;
+
+public class VectorsDistancesCalculator {
+}
