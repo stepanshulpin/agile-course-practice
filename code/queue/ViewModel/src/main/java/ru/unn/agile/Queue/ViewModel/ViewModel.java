0acf05da5d66ba9d324001c0580222c1984e9e97
Kirill Iuferev <juferev18@gmail.com>
2019-12-05 14:15:16 +0300

    viewModel can add default values

--- code/queue/ViewModel/src/main/java/ru/unn/agile/Queue/ViewModel/ViewModel.java
+++ code/queue/ViewModel/src/main/java/ru/unn/agile/Queue/ViewModel/ViewModel.java
@@ -1,4 +1,19 @@
 package ru.unn.agile.Queue.viewmodel;
 
 public class ViewModel {
+    private String queueString;
+    private String inputElement;
+
+    public ViewModel() {
+        inputElement = "";
+        queueString = "";
+    }
+
+    public String getQueueString() {
+        return queueString;
+    }
+
+    public String getInputElement() {
+        return inputElement;
+    }
 }
--- code/queue/ViewModel/src/test/java/ru/unn/agile/Queue/viewmodel/ViewModelTests.java
+++ code/queue/ViewModel/src/test/java/ru/unn/agile/Queue/viewmodel/ViewModelTests.java
@@ -1,4 +1,29 @@
 package ru.unn.agile.Queue.viewmodel;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ViewModelTests {
+    private ViewModel viewModel;
+    @Before
+    public void creatingViewModel() {
+        viewModel = new ViewModel();
+    }
+
+    @After
+    public void deletingViewModel() {
+        viewModel = null;
+    }
+
+    @Test
+    public void canSetDefaultQueueValue() {
+        assertEquals("", viewModel.getQueueString());
+    }
+
+    @Test
+    public void canSetDefaultInputElement() {
+        assertEquals("", viewModel.getInputElement());
+    }
 }
