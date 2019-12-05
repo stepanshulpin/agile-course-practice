9840c622b6eada34ca0ede91f29acab17a1b6e31
Kirill Iuferev <juferev18@gmail.com>
2019-12-05 12:44:57 +0300

    changes for view

--- code/queue/View/src/main/java/ru/unn/agile/Queue/view/Queue.java
+++ code/queue/View/src/main/java/ru/unn/agile/Queue/view/Queue.java
@@ -1,4 +1,17 @@
 package ru.unn.agile.Queue.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Queue {
+    @FXML
+    private TextField queueNewElement;
+    @FXML
+    private Button addElement;
+    @FXML
+    private Button getHead;
+    @FXML
+    private Label queueResult;
 }
