d4d583c6426d6a6afe369c0f75e203155d404211
Max <thanatormax@gmail.com>
2019-12-07 18:13:29 +0300

    Style refactoring

--- code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorMetrics.java
+++ code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorMetrics.java
@@ -5,11 +5,18 @@ import java.util.Vector;
 public final class VectorMetrics {
 
     public static double calculateL1(final Vector<Double> vector) {
-        return vector.stream().map(Math::abs).reduce((x, y) -> x + y).get();
+        return vector.stream()
+                     .map(Math::abs)
+                     .reduce((x, y) -> x + y)
+                     .get();
     }
 
     public static double calculateL2(final Vector<Double> vector) {
-        return Math.sqrt(vector.stream().map(Math::abs).map(x -> x * x).reduce((x, y) -> x + y).get());
+        return Math.sqrt(vector.stream()
+                               .map(Math::abs)
+                               .map(x -> x * x)
+                               .reduce((x, y) -> x + y)
+                               .get());
     }
 
     private VectorMetrics() { }
