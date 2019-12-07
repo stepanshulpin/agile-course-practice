e1bb4478e2fceecc96095a38f0c3af37f485a3c5
Max <thanatormax@gmail.com>
2019-12-07 17:43:45 +0300

    Added exceptions tests in VectorMetricsTests

--- code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorMetricsTests.java
+++ code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorMetricsTests.java
@@ -2,6 +2,7 @@ package ru.unn.agile.vectorsdistancescalculator.model;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
@@ -17,6 +18,18 @@ public class VectorMetricsTests {
         float expectedL1 = 1.0f;
         float actualL1 = VectorMetrics.calculateL1(vector);
         assertEquals(expectedL1, actualL1, EPS);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void throwExceptionCalculateL1DistanceEmptyVector() {
+        Vector<Float> vector = new Vector<>();
+        VectorMetrics.calculateL1(vector);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwNPECalculateL1DistanceNullVector() {
+        Vector<Float> vector = null;
+        VectorMetrics.calculateL1(vector);
     }
 
 }
