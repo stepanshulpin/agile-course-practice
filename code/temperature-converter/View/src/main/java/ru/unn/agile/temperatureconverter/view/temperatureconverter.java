3f263fe42012e15d4904f4bc9597c572ac48717e
Julia Kulygina <julia.kulygina@vicuesoft.com>
2019-12-07 21:47:26 +0300

    Added connection view and viewmodel

--- code/temperature-converter/View/src/main/java/ru/unn/agile/temperatureconverter/view/temperatureconverter.java
+++ code/temperature-converter/View/src/main/java/ru/unn/agile/temperatureconverter/view/temperatureconverter.java
@@ -1,6 +1,10 @@
 package ru.unn.agile.temperatureconverter.view;
-
+import ru.unn.agile.temperatureconverter.viewmodel.ViewModel;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class temperatureconverter {
     private JLabel labelHello;
@@ -12,13 +16,52 @@ public class temperatureconverter {
     private JLabel labelResult;
     private JButton buttonConvert;
     private JLabel labelStatus;
-    private JTextField textField1;
+    private JTextField fromField;
+
+    private ViewModel viewModel;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("temperatureconverter");
-        frame.setContentPane(new temperatureconverter().MainPanel);
+        frame.setContentPane(new temperatureconverter(new ViewModel()).MainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public temperatureconverter(final ViewModel viewModel) {
+        this.viewModel = viewModel;
+        ViewModel.ListOfTemperatures[] operations = ViewModel.ListOfTemperatures.values();
+        comboBoxFrom.setModel(new JComboBox<>(operations).getModel());
+        comboBoxTo.setModel(new JComboBox<>(operations).getModel());
+
+        backBind();
+        buttonConvert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                bind();
+                temperatureconverter.this.viewModel.calculate();
+                backBind();
+            }
+        });
+
+        KeyAdapter keyListener = new KeyAdapter() {
+            public void keyReleased(final KeyEvent e) {
+                bind();
+                temperatureconverter.this.viewModel.processInput();
+                backBind();
+            }
+        };
+        fromField.addKeyListener(keyListener);
+    }
+    private void backBind() {
+        buttonConvert.setEnabled(viewModel.isConvertButtonEnabled());
+        labelResult.setText(viewModel.getResultTemperature());
+        labelStatus.setText(viewModel.getStatusText());
+    }
+
+    private void bind() {
+        viewModel.setFromTemperature(fromField.getText());
+        viewModel.setFrom((ViewModel.ListOfTemperatures) comboBoxFrom.getSelectedItem());
+        viewModel.setTo((ViewModel.ListOfTemperatures) comboBoxTo.getSelectedItem());
     }
 }
