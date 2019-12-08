c96b136e0968af2f583dfaab5b449db86837f519
Max <thanatormax@gmail.com>
2019-12-08 14:04:38 +0300

    Added test for L2 calculation for calculateLp in VectorsDistancesCalculator

--- code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
+++ code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
@@ -58,6 +58,27 @@ public class VectorsDistancesCalculatorTests {
         assertEquals(expectedL1, actualL1, EPS);
     }
 
+    @Test
+    public void canCalculateL2DistancePositiveElems() {
+        List<Double> x = new ArrayList<>(List.of(3.0, 2.0));
+        List<Double> y = new ArrayList<>(List.of(2.0, 1.0));
+        int power = 2;
+        double expectedL2 = Math.sqrt(2.0);
+        double actualL2 = VectorsDistancesCalculator.calculateLp(x, y, power);
+        assertEquals(expectedL2, actualL2, EPS);
+
+    }
+
+    @Test
+    public void canCalculateL2DistanceMixedElems() {
+        List<Double> x = new ArrayList<>(List.of(3.0, -2.0));
+        List<Double> y = new ArrayList<>(List.of(-2.0, 1.0));
+        int power = 2;
+        double expectedL2 = Math.sqrt(34);
+        double actualL2 = VectorsDistancesCalculator.calculateLp(x, y, power);
+        assertEquals(expectedL2, actualL2, EPS);
+    }
+
 
 
 }
