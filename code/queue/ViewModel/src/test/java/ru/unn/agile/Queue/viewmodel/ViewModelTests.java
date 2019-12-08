84f9950dd47574b8db25b86dcb22275b486ccdb8
Kirill Iuferev <juferev18@gmail.com>
2019-12-08 23:11:55 +0300

    [refactoring]: some tests was added

--- code/queue/ViewModel/src/test/java/ru/unn/agile/Queue/viewmodel/ViewModelTests.java
+++ code/queue/ViewModel/src/test/java/ru/unn/agile/Queue/viewmodel/ViewModelTests.java
@@ -60,54 +60,112 @@ public class ViewModelTests {
     @Test
     public void canPushElementToQueue() {
         String expectedStatusValue = "Push element: 3.0";
+
         viewModel.setQueueInputElement("3");
         viewModel.pushProcess();
-
         var actualStatusValue = viewModel.getQueueResult();
+
+        assertEquals(expectedStatusValue, actualStatusValue);
+    }
+
+    @Test
+    public void canPushMoreThenOneElementToQueue() {
+        String expectedStatusValue = "Push element: 5.0";
+
+        viewModel.setQueueInputElement("3");
+        viewModel.pushProcess();
+        viewModel.setQueueInputElement("5");
+        viewModel.pushProcess();
+        var actualStatusValue = viewModel.getQueueResult();
+
         assertEquals(expectedStatusValue, actualStatusValue);
     }
 
     @Test
     public void canPopElementFromQueue() {
         String expectedStatusValue = "Pop element: 3.0";
+
         viewModel.setQueueInputElement("3");
         viewModel.pushProcess();
         viewModel.popProcess();
-
         var actualStatusValue = viewModel.getQueueResult();
+
         assertEquals(expectedStatusValue, actualStatusValue);
     }
 
     @Test
-    public void canClearQueue() {
+    public void canNotPopElementFromQueue() {
+        String expectedStatusValue = "Queue is empty.";
+
+        viewModel.popProcess();
+        var actualStatusValue = viewModel.getQueueResult();
+
+        assertEquals(expectedStatusValue, actualStatusValue);
+    }
+
+    @Test
+    public void canClearQueueWithElements() {
         String expectedStatusValue = "Queue cleared";
+
         viewModel.setQueueInputElement("3");
         viewModel.pushProcess();
         viewModel.clearProcess();
-
         var actualStatusValue = viewModel.getQueueResult();
+
+        assertEquals(expectedStatusValue, actualStatusValue);
+    }
+
+    @Test
+    public void canClearEmptyQueue() {
+        String expectedStatusValue = "Queue cleared";
+
+        viewModel.clearProcess();
+        var actualStatusValue = viewModel.getQueueResult();
+
         assertEquals(expectedStatusValue, actualStatusValue);
     }
 
     @Test
     public void canGetHeadFromQueue() {
         String expectedStatusValue = "Head is: 3.0";
+
         viewModel.setQueueInputElement("3");
         viewModel.pushProcess();
         viewModel.getHeadProcess();
-
         var actualStatusValue = viewModel.getQueueResult();
+
+        assertEquals(expectedStatusValue, actualStatusValue);
+    }
+
+    @Test
+    public void canNotGetHeadFromQueue() {
+        String expectedStatusValue = "Queue is empty.";
+
+        viewModel.getHeadProcess();
+        var actualStatusValue = viewModel.getQueueResult();
+
         assertEquals(expectedStatusValue, actualStatusValue);
     }
 
     @Test
     public void canGetTailFromQueue() {
         String expectedStatusValue = "Tail is: 3.0";
+
         viewModel.setQueueInputElement("3");
         viewModel.pushProcess();
         viewModel.getTailProcess();
-
         var actualStatusValue = viewModel.getQueueResult();
+
+        assertEquals(expectedStatusValue, actualStatusValue);
+    }
+
+    @Test
+    public void canNotGetTailFromEmptyQueue() {
+        String expectedStatusValue = "Queue is empty.";
+
+        viewModel.getTailProcess();
+        var actualStatusValue = viewModel.getQueueResult();
+
         assertEquals(expectedStatusValue, actualStatusValue);
     }
 }
