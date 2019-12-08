dc6f9978f6bc6b5b3ce0f9fed269e811812f637c
Kirill Iuferev <juferev18@gmail.com>
2019-12-08 21:06:24 +0300

    view changed. Added some needed elements

--- code/queue/View/src/main/java/ru/unn/agile/Queue/view/Queue.java
+++ code/queue/View/src/main/java/ru/unn/agile/Queue/view/Queue.java
@@ -4,14 +4,27 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import ru.unn.agile.Queue.viewmodel.ViewModel;
 
 public class Queue {
+
+    @FXML
+    private ViewModel viewModel;
     @FXML
     private TextField queueNewElement;
     @FXML
     private Button addElement;
     @FXML
+    private Button popElement;
+    @FXML
+    private Button clearQueue;
+    @FXML
     private Button getHead;
     @FXML
+    private Button getTail;
+    @FXML
     private Label queueResult;
+    @FXML
+    private Label status;
+
 }
