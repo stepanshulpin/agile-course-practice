3e1e90eb426458472d75165e15ee822b3bbbeb03
Max <thanatormax@gmail.com>
2019-12-08 13:59:17 +0300

    Added test for L1 calculation for calculateLp in VectorsDistancesCalculator

--- code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculator.java
+++ code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculator.java
@@ -18,14 +18,17 @@ public final class VectorsDistancesCalculator {
         if (x.size() != y.size()) {
             throw new IllegalArgumentException("Vectors must have same dimension");
         }
+        List<Double> dif = calculateVectorsDifference(x, y);
+        return VectorMetrics.calculateLp(dif, power);
+    }
 
+    private static List<Double> calculateVectorsDifference(final List<Double> x, final List<Double> y) {
         List<Double> dif = new ArrayList<>();
         for (int i = 0; i < x.size(); ++i) {
             dif.add(y.get(i) - x.get(i));
         }
-        return VectorMetrics.calculateLp(dif, power);
+        return dif;
     }
-
 
     private VectorsDistancesCalculator() { }
 
--- code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
+++ code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static org.junit.Assert.assertEquals;
+
+
 public class VectorsDistancesCalculatorTests {
+
+    private static final double EPS = 0.01f;
 
     @Test(expected = IllegalArgumentException.class)
     public void throwExceptionIfVectorsHaveDifferentDimensions() {
@@ -30,6 +35,27 @@ public class VectorsDistancesCalculatorTests {
         List<Double> y = new ArrayList<>();
         int power = 1;
         VectorsDistancesCalculator.calculateLp(x, y, power);
+    }
+
+    @Test
+    public void canCalculateL1DistancePositiveElems() {
+        List<Double> x = new ArrayList<>(List.of(3.0, 2.0));
+        List<Double> y = new ArrayList<>(List.of(2.0, 1.0));
+        int power = 1;
+        double expectedL1 = 2.0;
+        double actualL1 = VectorsDistancesCalculator.calculateLp(x, y, power);
+        assertEquals(expectedL1, actualL1, EPS);
+
+    }
+
+    @Test
+    public void canCalculateL1DistanceMixedElems() {
+        List<Double> x = new ArrayList<>(List.of(3.0, -2.0));
+        List<Double> y = new ArrayList<>(List.of(-2.0, 1.0));
+        int power = 1;
+        double expectedL1 = 8.0;
+        double actualL1 = VectorsDistancesCalculator.calculateLp(x, y, power);
+        assertEquals(expectedL1, actualL1, EPS);
     }
 
 
