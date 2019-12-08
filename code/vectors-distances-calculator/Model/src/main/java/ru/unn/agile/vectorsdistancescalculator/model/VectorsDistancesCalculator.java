e517f4c350516c17db1e9a918b4764f34af97c89
Max <thanatormax@gmail.com>
2019-12-08 13:49:30 +0300

    Exception tests for calculateLp in VectorsDistancesCalculator added

--- code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculator.java
+++ code/vectors-distances-calculator/Model/src/main/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculator.java
@@ -1,7 +1,31 @@
 package ru.unn.agile.vectorsdistancescalculator.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public final class VectorsDistancesCalculator {
+
+    public static double calculateLp(final List<Double> x,
+                                     final List<Double> y, final int power) {
+        if (x == null || y == null) {
+            throw new NullPointerException("Vector(s) can't be null");
+        }
+        if (x.isEmpty() || y.isEmpty()) {
+            throw new NoSuchElementException("Vector(s) can't be empty");
+        }
+        if (x.size() != y.size()) {
+            throw new IllegalArgumentException("Vectors must have same dimension");
+        }
+
+        List<Double> dif = new ArrayList<>();
+        for (int i = 0; i < x.size(); ++i) {
+            dif.add(y.get(i) - x.get(i));
+        }
+        return VectorMetrics.calculateLp(dif, power);
+    }
+
 
     private VectorsDistancesCalculator() { }
 
--- code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
+++ code/vectors-distances-calculator/Model/src/test/java/ru/unn/agile/vectorsdistancescalculator/model/VectorsDistancesCalculatorTests.java
@@ -1,6 +1,37 @@
 package ru.unn.agile.vectorsdistancescalculator.model;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class VectorsDistancesCalculatorTests {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwExceptionIfVectorsHaveDifferentDimensions() {
+        List<Double> x = new ArrayList<>(List.of(1.0, 2.0));
+        List<Double> y = new ArrayList<>(List.of(1.0));
+        int power = 1;
+        VectorsDistancesCalculator.calculateLp(x, y, power);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void throwExceptionIfVectorsEmpty() {
+        List<Double> x = new ArrayList<>(List.of(1.0));
+        List<Double> y = new ArrayList<>();
+        int power = 1;
+        VectorsDistancesCalculator.calculateLp(x, y, power);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void throwExceptionIfVectorNull() {
+        List<Double> x = null;
+        List<Double> y = new ArrayList<>();
+        int power = 1;
+        VectorsDistancesCalculator.calculateLp(x, y, power);
+    }
+
 
 
 }
