eeeaffdf043c70589691984ccbfa014409ff4669
antonyvlasov332 <antonybiggestbro@gmail.com>
2019-12-14 13:41:07 +0300

    remove unused import

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AntonVlasovTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AntonVlasovTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import ru.unn.agile.ComplexNumber.model.ComplexNumber;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class AntonVlasovTest {
     @Test(expected = NumberFormatException.class)
